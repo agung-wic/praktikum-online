0bbd46c1685212a7952fa97707469a69c0a318a8
dws16 <dauuuussss@gmail.com>
2020-07-13 21:21:34 +0700

    benerin background

--- registrasi.php
+++ registrasi.php
@@ -7,8 +7,7 @@
     <title>Registrasi</title>
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="login.css">
 </head>
 
@@ -16,6 +15,8 @@
     body {
         background-image: url(img/background.png);
         margin-top: 125px;
+        background-repeat: no-repeat;
+        background-size: cover;
     }
 
     .container {
@@ -46,24 +47,19 @@
         <!-- Form -->
         <form>
             <div class="form-group">
-                <input class="form-control" type="text" placeholder="Email"
-                    style="margin-top: 35px; background-color: white; border-top: 0px; border-bottom: 1px solid #6a6a6a; border-left: 1px solid #6a6a6a; border-right: 0px;">
+                <input class="form-control" type="text" placeholder="Email" style="margin-top: 35px; background-color: white; border-top: 0px; border-bottom: 1px solid #6a6a6a; border-left: 1px solid #6a6a6a; border-right: 0px;">
             </div>
             <div class="form-group">
-                <input class="form-control" type="text" placeholder="Nama"
-                    style="margin-top: 25px; background-color: white; border-top: 0px; border-bottom: 1px solid #6a6a6a; border-left: 1px solid #6a6a6a; border-right: 0px;">
+                <input class="form-control" type="text" placeholder="Nama" style="margin-top: 25px; background-color: white; border-top: 0px; border-bottom: 1px solid #6a6a6a; border-left: 1px solid #6a6a6a; border-right: 0px;">
             </div>
             <div class="form-group">
-                <input class="form-control" type="text" placeholder="NRP"
-                    style="margin-top: 25px; background-color: white; border-top: 0px; border-bottom: 1px solid #6a6a6a; border-left: 1px solid #6a6a6a; border-right: 0px;">
+                <input class="form-control" type="text" placeholder="NRP" style="margin-top: 25px; background-color: white; border-top: 0px; border-bottom: 1px solid #6a6a6a; border-left: 1px solid #6a6a6a; border-right: 0px;">
             </div>
             <div class="form-group">
-                <input class="form-control" type="password" placeholder="Password"
-                    style="margin-top: 25px; background-color: white; border-top: 0px; border-bottom: 1px solid #6a6a6a; border-left: 1px solid #6a6a6a; border-right: 0px;">
+                <input class="form-control" type="password" placeholder="Password" style="margin-top: 25px; background-color: white; border-top: 0px; border-bottom: 1px solid #6a6a6a; border-left: 1px solid #6a6a6a; border-right: 0px;">
             </div>
             <div class="form-group">
-                <input class="form-control" type="password" placeholder="Konfirmasi Password"
-                    style="margin-top: 25px; background-color: white; border-top: 0px; border-bottom: 1px solid #6a6a6a; border-left: 1px solid #6a6a6a; border-right: 0px;">
+                <input class="form-control" type="password" placeholder="Konfirmasi Password" style="margin-top: 25px; background-color: white; border-top: 0px; border-bottom: 1px solid #6a6a6a; border-left: 1px solid #6a6a6a; border-right: 0px;">
             </div>
             <button type="submit" class="btn" style="display: block; margin: auto; background-color: #6a6a6a; color: white; border-top-left-radius: 25px;
             border-top-right-radius: 25px;
@@ -74,14 +70,11 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
-        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
     </script>
 </body>
 
