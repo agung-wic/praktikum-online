b3b4c6c4bd2a882a3bfd66675958784d63e75306
mpuhambyah <mpu.hambyah@gmail.com>
2020-07-14 01:58:21 +0700

    Login & Daftar

---
+++ application/views/auth/registration.php
@@ -0,0 +1,43 @@
+<div class="container">
+    <div class="card o-hidden border-0 shadow-lg my-5  col-lg-7 mx-auto">
+        <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <img src="<?php echo base_url(); ?>assets/img/registration_user.png" width="100" height="100" style="display: block; margin: auto; margin-top:25px" />
+            <div class="row">
+                <div class="col-lg">
+                    <div class="p-5">
+                        <div class="text-center">
+                            <h1 class="h4 text-gray-900 mb-4">Formulir Pendaftaran Praktikum</h1>
+                        </div>
+                        <form class="user" method="post" action="<?= base_url('auth/registration'); ?>">
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Nama" value="<?= set_value('name'); ?>">
+                                <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
+                            </div>
+                            <div class=" form-group">
+                                <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email" value="<?= set_value('email'); ?>">
+                                <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
+                            </div>
+                            <div class="form-group row">
+                                <div class="col-sm-6 mb-3 mb-sm-0">
+                                    <input type="password" class="form-control form-control-user" id="password1" name="password1" placeholder="Password">
+                                    <?= form_error('password1', '<small class="text-danger pl-3">', '</small>'); ?>
+                                </div>
+                                <div class="col-sm-6">
+                                    <input type="password" class="form-control form-control-user" id="password2" name="password2" placeholder="Repeat Password">
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-user btn-block">
+                                Register Account
+                            </button>
+                        </form>
+                        <hr>
+                        <div class="text-center" style="padding-top: 10px">
+                            <a class="small" href="<?= base_url('auth'); ?>">Already have an account? Login!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
