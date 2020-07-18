604763e34859aa822dbd62a389727e61b3e78e04
Nia Angellina <68159862+niaangellina@users.noreply.github.com>
2020-07-18 16:19:43 +0700

    Add files via upload

---
+++ percobaan4.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Custom fonts for this template-->
+  <link href="http://localhost/fisdas/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="http://localhost/fisdas/assets/css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="http://localhost/fisdas/assets/css/css.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+  <link rel="icon" href="http://localhost/fisdas//assets/img/logoits.png" type="image/gif">
+
+
+  <title>Percobaan Modul 4</title>
+</head>
+<style>
+  .begron {
+    background-image: url("http://localhost/fisdas//assets/img/background.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+  }
+</style>
+
+<body>
+  <div class="begron" id="wrapper"><div class="row mx-5">
+  <div class="col justify-content-center">
+    <div class="kotak">
+      <div class="container">
+
+        <h4 class="mt-3"><b>Gerak Peluru</b></h4>
+        <h6 class="mb-5">Kode Percobaan: M1</h6>
+        <h6><b>I. Tujuan Percobaan</b></h6>
+        <p>Mempelajari konsep gerak peluru dari suatu benda.</p>
+
+        <h6><b>II. Peralatan Yang Digunakan</b></h6>
+        <ol>
+          <li>Satu set alat <i>projectilelauncher</i></li>
+          <li>Proyektil</li>
+          <li>Pasir Silika</li>
+          <li>Mistar</li>
+          <li>Stopwatch</li>
+        </ol>
+
+        <h6><b>III. Teori</b></h6>
+        <p>Lintasan yang dilalui oleh peluru yang ditembakkan dari suatu alat penembak disebut trayektori. Trayektori ini dipengaruhi oleh gesekan udara tetapi untuk mempermudah persoalan dalam percobaan ini, gesekan udara diabaikan, sehingga benda selama bergerak hanya dipengaruhi oleh gaya berat (m.g)-nya saja. Bentuk lintasan trayektori memenuhi persamaan parabola, sehingga gerak peluru sering disebut dengan gerak parabola. Gambar 4.4. menunjukkan trayektori gerak peluru yang ditembakkan dengan sudut elevasi 0o.</p>
+        <div class="row justify-content-center mb-4">
+          <img src="http://localhost/fisdas/assets/img/modul4-1.png" width="80%" alt="">
+        </div>
+        <p>Selama peluru bergerak hanya gaya berat yang mempengaruhinya, dan menurut <b>Hukum II Newton</b>, komponen-komponen gaya dalam arah tegak lurus adalah:</p>
+        <div class="row justify-content-center mb-4">
+          <img src="http://localhost/fisdas/assets/img/modul4-2.png" width="80%" alt="">
+        </div>
+        <p>Oleh karena itu:</p>
+        <div class="row justify-content-center mb-4">
+          <img src="http://localhost/fisdas/assets/img/modul4-3.png" width="80%" alt="">
+        </div>
+        <p>Jika peluru bergerak dengan kecepatan awal (vo) dan membentuk sudut elevasinya (0o), maka kecepatan peluru pada saat t adalah :</p>
+        <div class="row justify-content-center mb-4">
+          <img src="http://localhost/fisdas/assets/img/modul4-4.png" width="80%" alt="">
+        </div>
+        <p>Besarnya kecepatan pada setiap saat adalah:</p>
+        <div class="row justify-content-center mb-4">
+          <img src="http://localhost/fisdas/assets/img/modul4-5.png" width="80%" alt="">
+        </div>
+        <p>dan sudut arah kecepatan terhadap arah horizontal adalah:</p>
+        <div class="row justify-content-center mb-4">
+          <img src="http://localhost/fisdas/assets/img/modul4-6.png" width="80%" alt="">
+        </div>
+        <p>Oleh karena kecepatan pada arah sumbu x konstan, maka koordinat pada saat sembarang adalah:</p>
+        <div class="row justify-content-center mb-4">
+          <img src="http://localhost/fisdas/assets/img/modul4-7.png" width="80%" alt="">
+        </div>
+        <p>dan koordinat y pada saat sembarang adalah:</p>
+        <div class="row justify-content-center mb-4">
+          <img src="http://localhost/fisdas/assets/img/modul4-8.png" width="80%" alt="">
+        </div>
+
+        <h6><b>IV. Cara Melakukan Percobaan:</b></h6>
+        <ol>
+          <li>Siapkan peralatan <i>projectile launcher</i>.</li>
+          <li>Rangkailah peralatan seperti Gambar 4.5.
+            <div class="row justify-content-center mb-4">
+                <img src="http://localhost/fisdas/assets/img/modul4-9.png" width="80%" alt="">
+            </div>
+          </li>
+          <li>Atur sudut elevasi (0o) dan tahan dengan penahan sudut yang telah disediakan, sesuai dengan briefing asisten.</li>
+          <li>Atur ketinggian landasan permukaan pasir silika pada wadah rangkaian peralatan projectile launcher sesuai dengan ketinggian pelontar.</li>
+          <li>Masukan beban berupa proyektil ke dalam pelontar.</li>
+          <li>Tembakkan peluru dengan jalan menarik pelatuk tembak.</li>
+          <li>Gunakan stopwatch untuk menentukan waktu (t) ketika peluru ditembakkan sampai peluru mengenai landasan permukaan pasir silika.</li>
+          <li>Gunakan mistar untuk menentukan jarak horizontal (s) yang ditempuh peluru.</li>
+          <li>Ulangi percobaan di atas sebanyak 5 kali.</li>
+          <li>Lakukan percobaan di atas dengan vo berbeda dengan jalan menarik pelatuk penembak pada jarak yang berbeda.</li>
+          <li>Lakukan percobaan di atas dengan sudut elevasi (o)yang berbeda.</li>
+        </ol>
+
+        <h6><b>V. Tugas Untuk Laporan Resmi:</b></h6>
+        <ol>
+          <li>Tentukan nilai kecepatan awal (vo) untuk masing-masing percobaan.</li>
+          <li>Tentukan tinggi maksimum (hmax) dari masing-masing percobaan.</li>
+          <li>Tentukan kecepatan (v) pada saat mengenai landasan permukaan pasir silika.</li>
+          <li>Buat kesimpulan dari hasil pengamatan dan perhitungan percobaan ini.</li>
+        </ol>
+
+        <h6><b>VI. Tugas Pendahuluan:</b></h6>
+        <ol>
+          <li>Tunjukkan bahwa trayektori gerak peluru adalah parabola, beserta contoh fenomena gerak parabola dalam kehidupan sehari-hari.</li>
+          <li>Tunjukkan persamaan komponen posisi dan kecepatan dalam gerak peluru.</li>
+          <li>Tunjukkan persamaan waktu untuk mencapai tinggi maksimum (hmax).</li>
+          <li>Apakah berat peluru berpengaruh terhadap jarak, ketinggian, dan kecepatan dari peluru, jelaskan!</li>
+          <li>Apakah sudut lemparan (0), kecepatan awal (vo), ketinggian awal (yo) berpengaruh terhadap jarak dan waktu tempuh dari peluru, jelaskan!</li>
+        </ol>
+      </div>
+    </div>
+  </div>
+  <div class="col justify-content-center">
+    <div class="kotak">
+      <div class="container">
+        <img name="main" style="display: block; margin: auto; background-color: #6a6a6a; color: white; border-top-left-radius: 25px;
+                border-top-right-radius: 25px;
+                border-bottom-left-radius: 25px;
+                border-bottom-right-radius: 25px;" id="main" width="640" height="360" src="http://192.168.0.176:8081">
+        <div class="row">
+          <div class="col-lg-8">
+            <div class="row">
+              <div class="col-lg-6">
+                <div class="kotak" style="background-color: #bcaead;">
+                  <div class="container mt-2" style="color: black;">
+                    <h6 class="mb-3"><b>Input</b></h6>
+                    <form action="">
+                      <div class="form-group">
+                        <label for="var1">Variabel 1</label>
+                        <input type="text" id="var1" name="var1" class="form-control form-control-user" id="var1" name="var1">
+                      </div>
+                      <div class="form-group">
+                        <label for="var2">Variabel 2</label>
+                        <input type="text" id="var2" name="var2" class="form-control form-control-user" id="var2" name="var2">
+                      </div>
+                      <div class="form-group">
+                        <label for="var3">Variabel 3</label>
+                        <input type="text" id="var3" name="var3" class="form-control form-control-user" id="var3" name="var3">
+                      </div>
+                      <div class="row justify-content-center mb-3">
+                        <button type="submit" class="btn btn-secondary px-4">
+                          Kirim
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <div class="kotak" style="background-color: #bcaead;">
+                  <div class="container mt-2" style="color: black;">
+                    <h6 class="mb-3"><b>Output</b></h6>
+                    <form action="">
+                      <div class="form-group">
+                        <label for="data1">Data 1</label>
+                        <input type="text" id="data1" name="data1" class="form-control form-control-user" id="data1" name="data1">
+                      </div>
+                      <div class="form-group">
+                        <label for="data2">Data 2</label>
+                        <input type="text" id="data2" name="data2" class="form-control form-control-user" id="data2" name="data2">
+                      </div>
+                      <div class="form-group">
+                        <label for="data3">Data 3</label>
+                        <input type="text" id="data3" name="data3" class="form-control form-control-user" id="data3" name="data3">
+                      </div>
+                      <div class="row justify-content-center mb-3">
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-auto ml-auto">
+            <h6><b>Waktu : 00:30:00</b></h6>
+          </div>
+        </div>
+        <div class="row mt-5">
+          <div class="col-lg-auto ml-auto">
+            <a href="#" class="btn btn-secondary px-5">Selesai</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div><!-- Logout Modal-->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Yakin keluar?</h5>
+        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">×</span>
+        </button>
+      </div>
+      <div class="modal-body">Pilih "Logout" jika ingin mengakhiri sesi ini.</div>
+      <div class="modal-footer">
+        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+        <a class="btn btn-primary" href="http://localhost/fisdas/auth/logout">Logout</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<!-- Bootstrap core JavaScript-->
+<script src="http://localhost/fisdas/assets/vendor/jquery/jquery.min.js"></script>
+<script src="http://localhost/fisdas/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="http://localhost/fisdas/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="http://localhost/fisdas/assets/js/sb-admin-2.min.js"></script>
+<script src="http://localhost/fisdas/assets/js/script.js"></script>
+</body>
+
+</html>
