6da1bda6c9725d6b237579563d62fdae69d0c63b
Nia Angellina <68159862+niaangellina@users.noreply.github.com>
2020-08-02 22:43:14 +0700

    verif email dan ganti pass

---
+++ verifikasi.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verifikasi Akun E-Mail</title>
+
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            height: 120px;
+            position: fixed;
+            background-image: url(img/background2.jpeg);
+            width: 100%;
+            top: 0;
+        }
+
+        .logo img {
+            width: 50px;
+            height: 50px;
+            float: left;
+            margin-right: 10px;
+            margin-top: 35px;
+            margin-left: 25px;
+        }
+
+        .logo h5 {
+            font-weight: bold;
+            color: #707070;
+            padding-top: 35px;
+        }
+
+        .logo p {
+            color: #717171;
+            margin-top: -15px;
+        }
+
+        section {
+            margin-top: 195px;
+            margin-left: auto;
+            margin-right: auto;
+            background-color: #f6f6f6;
+            padding: 20px;
+            width: 30%;
+            border-top-left-radius: 25px;
+            border-top-right-radius: 25px;
+            border-bottom-left-radius: 25px;
+            border-bottom-right-radius: 25px;
+        }
+
+        .wrapper h4 {
+            color: #270d10;
+            font-weight: bold;
+        }
+
+        .wrapper p {
+            color: #5d4e4f;
+        }
+
+        .wrapper button {
+            display: block;
+            margin: auto;
+            padding-left: 30px;
+            padding-right: 30px;
+            border-top-left-radius: 25px;
+            border-top-right-radius: 25px;
+            border-bottom-left-radius: 25px;
+            border-bottom-right-radius: 25px;
+        }
+
+        footer {
+            position: fixed;
+            width: 100%;
+            height: 120px;
+            background-image: url(img/background2.jpeg);
+            bottom: 0;
+        }
+
+        .foot p {
+            padding-top: 13px;
+            padding-right: 25px;
+        }
+    </style>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+
+<body>
+    <header>
+        <div class="logo">
+            <img src="img/logoits.png" alt="Logo ITS">
+            <h5>Praktikum Laboratorium Fisika</h5>
+            <p>Institut Teknologi Sepuluh Nopember</p>
+        </div>
+    </header>
+
+    <section class="content">
+        <div class="wrapper">
+            <h4>Verifikasi Akun E-Mail</h4>
+            <hr>
+            <p>Hi Agung,</p>
+            <p>Terimakasih telah membuat akun di virtulab-its.com. Klik tombol dibawah ini untuk
+                memverifikasi akun E-Mail
+                anda.</p>
+            <button type="button" class="btn btn-secondary">Konfirmasi</button>
+        </div>
+    </section>
+
+    <footer>
+        <div class="foot">
+            <p class="text-uppercase text-right">laboratorium fisika dasar <br>departemen fisika <br>fakultas sains dan
+                analitika
+                data <br>institut teknologi sepuluh nopember</p>
+        </div>
+    </footer>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src=" https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
