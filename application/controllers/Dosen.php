3ec2bf2ac22b4344fed8df6c983b65c1326c68a6
dws16 <dauuuussss@gmail.com>
2020-08-11 03:29:42 +0700

    typo

--- application/controllers/Dosen.php
+++ application/controllers/Dosen.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Asisten extends CI_Controller
+class Dosen extends CI_Controller
 {
   public function __construct()
   {
