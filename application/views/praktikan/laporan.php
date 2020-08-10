9aec1a129e8bdc89f502de44e965cf7e16fb3252
dws16 <dauuuussss@gmail.com>
2020-08-10 22:52:12 +0700

    typo

--- application/views/praktikan/laporan.php
+++ application/views/praktikan/laporan.php
@@ -8,8 +8,8 @@
         </div>
         <div class="card-body">
           <div class="row">
+            <?= form_open_multipart(base_url('praktikan/uploadlaporan')) ?>
             <div class="col">
-              <?= form_open_multipart(base_url('praktikan/uploadlaporan')) ?>
 
               <div class="form-group">
                 <label for="modul">Pilih Modul</label>
@@ -23,13 +23,13 @@
                 <input type="file" class="custom-file-input" id="filelaporan" name="filelaporan">
                 <label class="custom-file-label" for="filelaporan">Pilih berkas</label>
               </div>
-              <div class="form-group row justify-content-end">
-                <div class="col-sm-10">
-                  <button type="submit" class="btn btn-primary">Unggah</button>
-                </div>
-              </div>
-              </form>
             </div>
+            <div class="form-group row justify-content-end">
+              <div class="col-sm-10">
+                <button type="submit" class="btn btn-primary">Unggah</button>
+              </div>
+            </div>
+            </form>
           </div>
         </div>
       </div>
