cca6031834e070aada104b996c300a3a59219151
dws16 <dauuuussss@gmail.com>
2020-08-11 01:31:26 +0700

    tes

--- application/controllers/Asisten.php
+++ application/controllers/Asisten.php
@@ -15,9 +15,9 @@ class Asisten extends CI_Controller
     $data['title'] = 'Penilaian';
     $data['user'] = $this->db->get_where('user', ['nrp' => $this->session->userdata('nrp')])->row_array();
     $data['list'] = $this->Asisten_model->TampilNilai();
-    var_dump($data['list']);
+    $asisten = $data['list'][0]['asisten'];
+    var_dump($asisten);
     die;
-    $asisten = $data['list']['asisten'];
     $data['list']['asisten'] = $this->db->query("SELECT `name` FROM `user` WHERE `nrp`= $asisten")->row_array();
 
     $this->load->view('template/header', $data);
