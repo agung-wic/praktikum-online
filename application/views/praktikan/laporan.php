e58424a7a011e87b316059a4f0fe531a1006654f
dws16 <dauuuussss@gmail.com>
2020-08-10 22:40:59 +0700

    ngecilin kotak

--- application/views/praktikan/laporan.php
+++ application/views/praktikan/laporan.php
@@ -1,22 +1,24 @@
 <div class="container">
-  <div class="kotak mt-5">
-    <!-- Page Heading -->
-    <h1 class="mt-2 mb-5" style="text-align: center;"><?= $title ?></h1>
+  <div class="col-lg-7">
+    <div class="kotak mt-5">
+      <!-- Page Heading -->
+      <h1 class="mt-2 mb-5" style="text-align: center;"><?= $title ?></h1>
 
-    <?= form_open_multipart(base_url('praktikan/uploadlaporan')) ?>
+      <?= form_open_multipart(base_url('praktikan/uploadlaporan')) ?>
 
-    <div class="form-group">
-      <label for="modul">Pilih Modul</label>
-      <select class="form-control" name="modul_id" id="modul_id">
-        <?php foreach ($modul as $m) : ?>
-          <option value="<?= $m['modul'] ?>"><?= $m['name'] ?></option>
-        <?php endforeach; ?>
-      </select>
+      <div class="form-group">
+        <label for="modul">Pilih Modul</label>
+        <select class="form-control" name="modul_id" id="modul_id">
+          <?php foreach ($modul as $m) : ?>
+            <option value="<?= $m['modul'] ?>"><?= $m['name'] ?></option>
+          <?php endforeach; ?>
+        </select>
+      </div>
+      <div class="custom-file">
+        <input type="file" class="custom-file-input" id="filelaporan" name="filelaporan">
+        <label class="custom-file-label" for="filelaporan">Pilih berkas</label>
+      </div>
+      </form>
     </div>
-    <div class="custom-file">
-      <input type="file" class="custom-file-input" id="filelaporan" name="filelaporan">
-      <label class="custom-file-label" for="filelaporan">Pilih berkas</label>
-    </div>
-    </form>
   </div>
 </div>
