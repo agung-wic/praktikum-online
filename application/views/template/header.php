2c692b20cc4f1d104aa37e567541fd49fb0a3afd
dws16 <dauuuussss@gmail.com>
2020-08-11 18:09:50 +0700

    tes

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -24,7 +24,7 @@
 </head>
 <style>
   .begron {
-    background-image: url("<?= base_url(); ?>/assets/img/begron1.png");
+    background-image: url("<?= base_url(); ?>/assets/img/begron2.png");
     background-repeat: no-repeat;
     background-size: cover;
     background-attachment: fixed;
