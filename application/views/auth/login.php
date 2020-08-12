512135254136e58e9941cace2f36893fed0a8ffb
mpuhambyah <mpu.hambyah@gmail.com>
2020-08-12 14:51:05 +0700

    login balik seperti semula

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,9 +1,40 @@
-<!-- particles.js container -->
-<div id="particles-js"></div>
+<div class="container">
+    <!-- Logo -->
+    <img class="logo" src="<?php echo base_url(); ?>assets/img/logoits.png" alt="Logo ITS">
 
-<!-- stats - count particles -->
-<div class="count-particles">
-    <span class="js-count-particles">--</span> particles
+    <!-- Judul -->
+    <h4 class="pt-3">Praktikum Laboratorium Fisika</h4>
+    <h6 class="pb-4">Institut Teknologi Sepuluh Nopember</h6>
+
+
+    <!-- Form -->
+    <?= $this->session->flashdata('message') ?>
+    <form class="user" method="post" action="<?= base_url('auth'); ?>">
+        <div class="form-group">
+            <input class="form-control" type="text" id="nrp" name="nrp" placeholder="NRP" value="<?= set_value('nrp'); ?>" style="margin-top: 35px; background-color: #f6f6f6; border-top: 0; border-bottom: 1px solid #6a6a6a; border-left: 1px solid #6a6a6a; border-right: 0;">
+            <?= form_error('nrp', '<small class="text-danger pl-3">', '</small>'); ?>
+        </div>
+        <div class="form-group">
+            <input class="form-control" type="password" id="password" name="password" placeholder="Password" style="margin-top: 35px; 
+            background-color: #f6f6f6; 
+            border-top: 0; 
+            border-bottom: 1px solid #6a6a6a; 
+            border-left: 1px solid #6a6a6a; 
+            border-right: 0;">
+            <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
+        </div>
+        <button type="submit" class="btn" style="display: block; margin: auto; background: linear-gradient(40deg, rgba(111,140,252,1) 8%, rgba(47,200,201,1) 100%); color: white; border-top-left-radius: 25px;
+                border-top-right-radius: 25px;
+                border-bottom-left-radius: 25px;
+                border-bottom-right-radius: 25px;
+                padding-left:15%;
+                padding-right:15%">Login
+        </button>
+    </form>
+    <h6 class="text-center pt-4">
+        <a href="<?= base_url('auth/forgotpassword'); ?>" style="color: #6a6a6a; font-size: small;">Lupa Password?</a>
+    </h6>
+    <h6 class="text-center">
+        <u><a href="<?= base_url('auth/registration'); ?>" style=" color: #6a6a6a; font-size: small;">Daftar Praktikum</a></u>
+    </h6>
 </div>
-
-<!-- particles.js lib (JavaScript CodePen settings): https://github.com/VincentGarreau/particles.js -->
