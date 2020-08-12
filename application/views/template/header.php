ba7ec8ef67bc381e4eaf73aedb617bd3d425c39a
dws16 <dauuuussss@gmail.com>
2020-08-12 16:19:37 +0700

    ganti css

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -10,8 +10,8 @@
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="<?= base_url(); ?>assets/css/sb-admin-2.min.css" rel="stylesheet">
-  <link href="<?= base_url(); ?>assets/css/css.css" rel="stylesheet">
+  <link href="<?= base_url(); ?>assets/css/sb-admin-2.min.css" type="text/css" rel="stylesheet">
+  <link href="<?= base_url(); ?>assets/css/css.css" type="text/css" rel="stylesheet">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
   <link rel="icon" href="<?= base_url() ?>/assets/img/logoits.png" type="image/gif">
   <script src="https://cdn.tiny.cloud/1/mokfxgxe7oi1t7k11vksrqmimtsqx9jw5cl87i29uwovlamh/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
