7ea46d98589d02a16efb02837e6a8f35b89e34d5
mpuhambyah <mpu.hambyah@gmail.com>
2020-08-14 23:00:57 +0700

    coba particle js

--- application/views/template/auth_footer.php
+++ application/views/template/auth_footer.php
@@ -7,6 +7,31 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
 </script>
 <script src="<?= base_url(); ?>assets/js/particle.js"></script>
+<!-- scripts -->
+<script src="<?= base_url(); ?>assets/js/particles.js"></script>
+<script>
+    var count_particles, stats, update;
+    stats = new Stats();
+    stats.setMode(0);
+    stats.domElement.style.position = "absolute";
+    stats.domElement.style.left = "0px";
+    stats.domElement.style.top = "0px";
+    document.body.appendChild(stats.domElement);
+    count_particles = document.querySelector(".js-count-particles");
+    update = function() {
+        stats.begin();
+        stats.end();
+        if (
+            window.pJSDom[0].pJS.particles &&
+            window.pJSDom[0].pJS.particles.array
+        ) {
+            count_particles.innerText =
+                window.pJSDom[0].pJS.particles.array.length;
+        }
+        requestAnimationFrame(update);
+    };
+    requestAnimationFrame(update);
+</script>
 </body>
 
 </html>
