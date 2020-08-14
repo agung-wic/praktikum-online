e29032ee4a89091f7dd61796a76c2526f3a4dbdb
mpuhambyah <mpu.hambyah@gmail.com>
2020-08-14 23:07:47 +0700

    typo

--- application/views/template/auth_footer.php
+++ application/views/template/auth_footer.php
@@ -6,9 +6,9 @@
 </script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
 </script>
-<script src="<?= base_url(); ?>assets/js/particle.js"></script>
 <!-- scripts -->
 <script src="<?= base_url(); ?>assets/js/particles.js"></script>
+<script src="<?= base_url(); ?>assets/js/app.js"></script>
 <script>
     var count_particles, stats, update;
     stats = new Stats();
