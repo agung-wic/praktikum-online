f7cca4ed7763ab587a24367f6b26b6e9b179e606
dws16 <dauuuussss@gmail.com>
2020-08-14 16:31:11 +0700

    ganti tata letak

--- application/views/praktikan/percobaan.php
+++ application/views/praktikan/percobaan.php
@@ -34,73 +34,71 @@
   </div>
   <div class="col-lg-5">
     <div class="kotak">
-      <div class="row">
-        <div class="col-lg-6">
-          <div class="kotak" style="background-color: #bcaead;">
-            <div class="container mt-2" style="color: black;">
-              <h6 class="mb-3"><b>Input</b></h6>
+      <div class="container">
 
-              <input type="text" name="aksi" value="data" hidden>
-              <input type="text" id="id" data-id="<?= $modul['modul']; ?>" name="id" value="<?= $modul['modul']; ?>" hidden>
-              <div class="form-group">
-                <label for="var">Variabel</label>
-                <select class="form-control" name="var" id="var">
-                  <option <?php //if (($this->input->post('var')) && $this->input->post('var') == "x") echo "selected"; 
-                          ?> data-send="[x,500]" value="x">Naik</option>
-                  <option <?php //if (($this->input->post('var')) && $this->input->post('var') == "z") echo "selected"; 
-                          ?> data-send="z" value="z">Turun</option>
-                </select>
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="kotak" style="background-color: #bcaead;">
+              <div class="container mt-2" style="color: black;">
+                <h6 class="mb-3"><b>Input</b></h6>
+
+                <input type="text" name="aksi" value="data" hidden>
+                <input type="text" id="id" data-id="<?= $modul['modul']; ?>" name="id" value="<?= $modul['modul']; ?>" hidden>
+                <div class="form-group">
+                  <label for="var">Variabel</label>
+                  <select class="form-control" name="var" id="var">
+                    <option <?php //if (($this->input->post('var')) && $this->input->post('var') == "x") echo "selected"; 
+                            ?> data-send="[x,500]" value="x">Naik</option>
+                    <option <?php //if (($this->input->post('var')) && $this->input->post('var') == "z") echo "selected"; 
+                            ?> data-send="z" value="z">Turun</option>
+                  </select>
+                </div>
               </div>
-            </div>
-            <div class="row justify-content-center mb-3">
-              <button type="submit" data-aksi="data" class="btn btn-secondary px-4 kirim1">
-                Kirim
-              </button>
-            </div>
-            <div class="row justify-content-center mb-3">
-              <button type="submit" data-aksi="data" class="btn btn-secondary px-4 kirim1">
-                Kirim
-              </button>
-            </div>
-
-            <input type="text" id="id" name="id" data-id="<?= $modul['modul']; ?>" value="<?= $modul['modul']; ?>" hidden>
-            <input type="text" name="aksi" value="jatuhkan" hidden>
-            <div class="row justify-content-center mb-3">
-              <button type="submit" data-aksi="jatuhkan" class="btn btn-secondary px-4 kirim2">
-                Jatuhkan
-              </button>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-6">
-          <div class="kotak" style="background-color: #bcaead;">
-            <div class="container mt-2" style="color: black;">
-              <h6 class="mb-3"><b>Output</b></h6>
-              <div class="form-group">
-                <label for="data1">Data 1</label>
-                <output type="text" id="data1" name="data1" class="form-control form-control-user mb-4" id="data1" name="data1"></output>
-                <output type="text" id="data2" name="data2" class="form-control form-control-user" id="data2" name="data2"></output>
-              </div>
-
               <div class="row justify-content-center mb-3">
+                <button type="submit" data-aksi="data" class="btn btn-secondary px-4 kirim1">
+                  Kirim
+                </button>
               </div>
 
+              <input type="text" id="id" name="id" data-id="<?= $modul['modul']; ?>" value="<?= $modul['modul']; ?>" hidden>
+              <input type="text" name="aksi" value="jatuhkan" hidden>
+              <div class="row justify-content-center mb-3">
+                <button type="submit" data-aksi="jatuhkan" class="btn btn-secondary px-4 kirim2">
+                  Jatuhkan
+                </button>
+              </div>
             </div>
           </div>
+          <div class="col-lg-6">
+            <div class="kotak" style="background-color: #bcaead;">
+              <div class="container mt-2" style="color: black;">
+                <h6 class="mb-3"><b>Output</b></h6>
+                <div class="form-group">
+                  <label for="data1">Data 1</label>
+                  <output type="text" id="data1" name="data1" class="form-control form-control-user mb-4" id="data1" name="data1"></output>
+                  <output type="text" id="data2" name="data2" class="form-control form-control-user" id="data2" name="data2"></output>
+                </div>
+
+                <div class="row justify-content-center mb-3">
+                </div>
+
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-auto ml-auto">
+            <h6><b>Sisa Waktu </h6> <span id="hours"></span>
+            <span id="mins"></span>
+            <span id="secs"></span>
+            </b>
+          </div>
         </div>
-        <div class="col-lg-auto ml-auto">
-          <h6><b>Sisa Waktu </h6> <span id="hours"></span>
-          <span id="mins"></span>
-          <span id="secs"></span>
-          </b>
-        </div>
-      </div>
-      <div class="row mt-5">
-        <div class="col-lg-auto mr-auto">
-          <a href="<?= base_url('praktikan/modul/') . $modul['modul'] ?>" class="btn btn-secondary px-5">Kembali</a>
-        </div>
-        <div class="col-lg-auto ml-auto">
-          <a href="<?= base_url('praktikan/modul/') ?>" class="btn gradien px-5">Selesai</a>
+        <div class="row mt-5">
+          <div class="col-lg-auto mr-auto">
+            <a href="<?= base_url('praktikan/modul/') . $modul['modul'] ?>" class="btn btn-secondary px-5">Kembali</a>
+          </div>
+          <div class="col-lg-auto ml-auto">
+            <a href="<?= base_url('praktikan/modul/') ?>" class="btn gradien px-5">Selesai</a>
+          </div>
         </div>
       </div>
     </div>
