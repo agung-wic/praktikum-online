58488b35362ac8476c8a963c1def911c3a7f28cb
dws16 <dauuuussss@gmail.com>
2020-08-15 16:49:55 +0700

    coba verif

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -112,13 +112,211 @@ class Auth extends CI_Controller
                     'image' => 'default.jpg',
                     'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
                     'role_id' => 2,
-                    'is_active' => 1,
+                    'is_active' => 0,
                     'date_created' => time()
                 ];
+                // token
+                $token = base64_encode(random_bytes(32));
+                $user_token = [
+                    'email' => $this->input->post('email', true),
+                    'token' => $token,
+                    'date_created' => time()
+                ];
+
                 $this->db->insert('user', $data);
-                $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Selamat akun berhasil terdaftar!</div>');
-                redirect('auth');
+                $this->db->insert('user_token', $user_token);
+
+                $this->_sendEmail($token, 'verify');
+
+                $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+            Selamat! Akun berhasil dibuat. Silakan periksa email untuk aktivasi!
+            </div>');
+                redirect(base_url('auth/login'));
             }
+        }
+    }
+
+    private function _sendEmail($token, $type, $data = NULL)
+    {
+        $config = [
+            'protocol' => 'smtp',
+            'smtp_host' => 'smtp.hostinger.co.id',
+            'smtp_user' => 'fisdas@virtulab-its.com',
+            'smtp_pass' => '1234asdf!@#$ASDF',
+            'smtp_port' => 587,
+            'mailtype' => 'html',
+            'charset' => 'utf-8',
+            'newline' => "\r\n"
+        ];
+
+        $this->load->library('email', $config);
+        $this->email->initialize($config);
+
+        $this->email->from('fisdas@virtulab-its.com', 'Praktikum Fisika Dasar');
+        $this->email->to($this->input->post('email', true));
+
+        if ($type == 'verify') {
+            $this->email->subject('Verifikasi Akun');
+            $this->email->message('<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <style>
+                    section {
+                        margin-top: 195px;
+                        margin-left: auto;
+                        margin-right: auto;
+                        background-color: #f6f6f6;
+                        padding: 20px;
+                        width: 30%;
+                        border-top-left-radius: 25px;
+                        border-top-right-radius: 25px;
+                        border-bottom-left-radius: 25px;
+                        border-bottom-right-radius: 25px;
+                    }
+                    .wrapper h4 {
+                        color: #270d10;
+                        font-weight: bold;
+                    }
+                    .wrapper p {
+                        color: #5d4e4f;
+                    }
+                    .wrapper button {
+                        display: block;
+                        margin: auto;
+                        padding-left: 30px;
+                        padding-right: 30px;
+                        border-top-left-radius: 25px;
+                        border-top-right-radius: 25px;
+                        border-bottom-left-radius: 25px;
+                        border-bottom-right-radius: 25px;
+                    }
+                </style>
+                <!-- Bootstrap CSS -->
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+            </head>
+            <section class="content">
+                <div class="wrapper">
+                    <h4>Verifikasi Akun E-Mail</h4>
+                    <hr>
+                    <p>Halo ' . $data["name"] . ',</p>
+                    <p>Terimakasih telah membuat akun di virtulab-its.com. Klik tombol dibawah ini untuk
+                        memverifikasi akun E-Mail
+                        anda.</p>
+                    <a href="' . base_url() . 'auth/verify?email=' . $this->input->post('email') . '&token=' . urlencode($token) . '" type="button" class="btn btn-secondary">Konfirmasi</>
+                </div>
+            </section>
+            <!-- Optional JavaScript -->
+            <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+            <script src=" https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+            </script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+            </script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+            </script>
+            </body>   
+            </html>');
+        } else if ($type == 'forgot') {
+            $this->email->subject('Reset Password');
+            $this->email->message('
+            
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <style>
+                    section {
+                        margin-top: 195px;
+                        margin-left: auto;
+                        margin-right: auto;
+                        background-color: #f6f6f6;
+                        padding: 20px;
+                        width: 30%;
+                        border-top-left-radius: 25px;
+                        border-top-right-radius: 25px;
+                        border-bottom-left-radius: 25px;
+                        border-bottom-right-radius: 25px;
+                    }
+                    .wrapper h4 {
+                        color: #270d10;
+                        font-weight: bold;
+                    }
+                    .wrapper p {
+                        color: #5d4e4f;
+                    }
+                    .wrapper button {
+                        display: block;
+                        margin: auto;
+                        padding-left: 30px;
+                        padding-right: 30px;
+                        border-top-left-radius: 25px;
+                        border-top-right-radius: 25px;
+                        border-bottom-left-radius: 25px;
+                        border-bottom-right-radius: 25px;
+                    }
+                </style>
+                <!-- Bootstrap CSS -->
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+            </head>
+            <section class="content">
+                <div class="wrapper">
+                    <h4>Konfirmasi Pergantian Password</h4>
+                    <hr>
+                    <p>Hi Agung,</p>
+                    <p>Klik tombol dibawah ini untuk mengonfirmasi pergantian password anda.</p>
+                    <a href="' . base_url() . 'auth/resetpassword?email=' . $this->input->post('email') . '&token=' . urlencode($token) . '" type="button" class="btn btn-secondary">Konfirmasi</a>
+                </div>
+            </section>
+            <!-- Optional JavaScript -->
+            <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+            <script src=" https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+            </script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+            </script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+            </script>
+            </body>   
+            </html>
+            
+            Click this link to reset your password : <a href="' . base_url() . 'auth/resetpassword?email=' . $this->input->post('email') . '&token=' . urlencode($token) . '">Reset Password</a>');
+        }
+
+        if ($this->email->send()) {
+            return true;
+        } else {
+            echo $this->email->print_debugger();
+            die;
+        }
+    }
+
+    public function verify()
+    {
+        $email = $this->input->get('email');
+        $token = $this->input->get('token');
+
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+        if ($user) {
+            $user_token = $this->db->get_where('user_token', ['token' => $token])->row_array();
+            if ($user_token) {
+                $this->db->set('is_active', 1);
+                $this->db->where('email', $email);
+                $this->db->update('user');
+
+                $this->db->delete('user_token', ['email' => $email]);
+                $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+                Akun sudah teraktivasi! Silakan masuk.
+                </div>');
+                redirect(base_url('auth'));
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                Aktivasi akun gagal! Token tidak valid.
+                </div>');
+                redirect(base_url('auth'));
+            }
+        } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+            Aktivasi akun gagal! Email salah.
+            </div>');
+            redirect(base_url('auth'));
         }
     }
 
@@ -132,45 +330,101 @@ class Auth extends CI_Controller
         redirect('auth');
     }
 
-    public function forgotpassword()
+    public function forgotPassword()
     {
-        if ($this->session->userdata('email')) {
-            redirect(base_url('profil'));
-        } else {
-            $data['title'] = 'Lupa Kata Sandi';
-            $this->form_validation->set_rules('email', 'Email', 'required|trim');
+        $data['title'] = 'Forgot Password';
 
-            if ($this->form_validation->run() == false) {
-                $this->load->view('template/auth_header.php', $data);
-                $this->load->view('auth/forgot-password');
-                $this->load->view('template/auth_footer.php');
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('template/auth_header', $data);
+            $this->load->view('auth/forgot-password');
+            $this->load->view('template/auth_footer', $data);
+        } else {
+            $email = $this->input->post('email', true);
+            $user = $this->db->get_where('user', ['email' => $email, 'is_active' => 1])->row_array();
+            if ($user) {
+                $token = base64_encode(random_bytes(32));
+                $user_token = [
+                    'email' => $email,
+                    'token' => $token,
+                    'date_created' => time()
+                ];
+
+                $this->db->insert('user_token', $user_token);
+                $this->_sendEmail($token, 'forgot');
+                $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+                Silakan periksa email untuk mengatur ulang kata sandi!
+                </div>');
+                redirect(base_url('auth/forgotpassword'));
             } else {
-                $email = $this->input->post('email');
-                $user = $this->db->get_where('user', ['email' => $email])->row_array();
-                if ($user) {
-                    #KIRIM EMAIL
-                    redirect('auth/changepassword');
-                } else {
-                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email salah</div>');
-                }
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                Email tidak terdaftar ataupun teraktivasi!
+                </div>');
+                redirect(base_url('auth/forgotpassword'));
             }
         }
     }
+
+    public function resetPassword()
+    {
+        $email = $this->input->get('email');
+        $token = $this->input->get('token');
+
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+        if ($user) {
+            $user_token = $this->db->get_where('user_token', ['token' => $token])->row_array();
+            if ($user_token) {
+                $this->session->set_userdata('reset_email', $email);
+                $this->changePassword();
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                Atur ulang kata sandi gagal! Token tidak valid.
+                </div>');
+                redirect(base_url('auth/login'));
+            }
+        } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+            Atur ulang kata sandi gagal! Email salah.
+                </div>');
+            redirect(base_url('auth/login'));
+        }
+    }
+
+    public function changePassword()
+    {
+        if (!$this->session->userdata('reset_email')) {
+            redirect(base_url('auth'));
+        }
+        $data['title'] = 'Ubah Kata Sandi';
+
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[8]|matches[password2]');
+        $this->form_validation->set_rules('password2', 'Repeat Password', 'required|trim|min_length[8]|matches[password1]');
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('template/auth_header', $data);
+            $this->load->view('auth/change-password');
+            $this->load->view('template/auth_footer', $data);
+        } else {
+            $password = password_hash($this->input->post('password1'), PASSWORD_DEFAULT);
+            $email = $this->session->userdata('reset_email');
+
+            $this->db->set('password', $password);
+            $this->db->where('email', $email);
+            $this->db->update('user');
+
+            $this->session->unset_userdata('reset_email');
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+                Kata sandi berhasil diubah! Silakan masuk.
+                </div>');
+            redirect(base_url('auth/login'));
+        }
+    }
+
     public function denied()
     {
         $data['title'] = 'Access Denied';
         $this->load->view('template/auth_header', $data);
         $this->load->view('auth/denied');
-    }
-    public function changepassword()
-    {
-        if ($this->session->userdata('email')) {
-            redirect(base_url('profil'));
-        } else {
-            $data['title'] = 'Ubah Kata Sandi';
-            $this->load->view('template/auth_header.php', $data);
-            $this->load->view('auth/change-password');
-            $this->load->view('template/auth_footer.php');
-        }
     }
 }
