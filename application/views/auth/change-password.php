c5a0e425d042091f36e4a80d38f7c5b8b363217b
dws16 <dauuuussss@gmail.com>
2020-08-15 22:35:45 +0700

    ganti warna button

--- application/views/auth/change-password.php
+++ application/views/auth/change-password.php
@@ -21,7 +21,7 @@
                 <input type="password" class="form-control form-control-user" id="password2" name="password2" placeholder="Ulangi kata sandi" style="margin-top: 35px; background-color: #f6f6f6; border-top: 0; border-bottom: 1px solid #6a6a6a; border-left: 1px solid #6a6a6a; border-right: 0;">
                 <?= form_error('password2', '<small class="text-danger pl-3">', '</small>'); ?>
             </div>
-            <button type="submit" class="btn" style="display: block; margin: auto; background-color: #6a6a6a; color: white; border-top-left-radius: 25px;
+            <button type="submit" class="btn" style="display: block; margin: auto; background: linear-gradient(40deg, rgba(111,140,252,1) 8%, rgba(47,200,201,1) 100%); color: white; border-top-left-radius: 25px;
                 border-top-right-radius: 25px;
                 border-bottom-left-radius: 25px;
                 border-bottom-right-radius: 25px;">
