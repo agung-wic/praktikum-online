9a6327659adadb70005b63d45c33d40f92846f22
mpuhambyah <mpu.hambyah@gmail.com>
2020-08-18 11:08:31 +0700

    test

--- application/models/Praktikan_model.php
+++ application/models/Praktikan_model.php
@@ -7,8 +7,8 @@ class Praktikan_model extends CI_Model
   {
     $query = "SELECT `jadwal`.`id`, `req_jadwal`.`is_approved`, `user`.`name` as 'name', `user`.`nrp`, 
               `modul`.`modul` as 'modul_id', `modul`.`name` as 'modul', `jadwal`.`jadwal`
-              FROM `user` LEFT JOIN `jadwal` ON `user`.`nrp` = `jadwal`.`nrp` 
-              LEFT JOIN `modul` ON `modul`.`modul` = `jadwal`.`modul_id`
+              FROM `user` INNER JOIN `jadwal` ON `user`.`nrp` = `jadwal`.`nrp` 
+              INNER JOIN `modul` ON `modul`.`modul` = `jadwal`.`modul_id`
               LEFT JOIN `req_jadwal` ON `jadwal`.`nrp`=`req_jadwal`.`nrp` AND `jadwal`.`modul_id` = `req_jadwal`.`modul_id` 
               WHERE `user`.`nrp`=$id LIMIT $limit OFFSET $start";
 
