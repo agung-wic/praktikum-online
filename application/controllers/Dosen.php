7f5a09ad041f09f4d90f590400c6c3778f6f0ceb
dws16 <dauuuussss@gmail.com>
2020-08-19 13:50:19 +0700

    debug

--- application/controllers/Dosen.php
+++ application/controllers/Dosen.php
@@ -168,7 +168,7 @@ class Dosen extends CI_Controller
 
     // Images upload path
     $imageFolder = $_SERVER['DOCUMENT_ROOT'] . "/assets/img/";
-
+    var_dump($_SERVER['DOCUMENT_ROOT']);
     reset($_FILES);
     $temp = current($_FILES);
     if (is_uploaded_file($temp['tmp_name'])) {
