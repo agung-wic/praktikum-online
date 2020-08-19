761849e86e841cbcd23fda382830ab2a1e7f544e
dws16 <dauuuussss@gmail.com>
2020-08-19 11:58:01 +0700

    ganti ukuran modal

--- application/views/dosen/modul.php
+++ application/views/dosen/modul.php
@@ -69,7 +69,7 @@
 </div>
 
 <div class="modal fade" id="BuatModul" tabindex="-1" role="dialog" aria-labelledby="BuatModulLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="BuatModulLabel">Buat Modul</h5>
