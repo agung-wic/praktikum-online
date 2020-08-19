4b18f5a1b41a32081238223a0c40ecfaf4c41abb
dws16 <dauuuussss@gmail.com>
2020-08-19 13:27:31 +0700

    debug

--- application/controllers/Dosen.php
+++ application/controllers/Dosen.php
@@ -167,7 +167,7 @@ class Dosen extends CI_Controller
     $accepted_origins = array("https://virtulab-its.com");
 
     // Images upload path
-    $imageFolder = base_url('assets/img');
+    $imageFolder = base_url('assets/img/');
 
     reset($_FILES);
     $temp = current($_FILES);
