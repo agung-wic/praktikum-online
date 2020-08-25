c4a1a125088a0fe48e810794d9fbedc565bf9378
dws16 <dauuuussss@gmail.com>
2020-08-25 12:55:14 +0700

    debug

--- application/views/praktikan/modul.php
+++ application/views/praktikan/modul.php
@@ -20,6 +20,8 @@
               <p class="mt-4"><b>Waktu : <?= $m['time'] ?></b></p>
               <p><b>Status : <?php
                               if ($i < count($status)) {
+                                var_dump($m['modul']);
+                                var_dump($status[$i]['modul']);
                                 if ($m['modul'] == $status[$i]['modul_id']) {
                                   if ($status[$i]['status'] == 0) {
                                     echo "Belum Selesai 1";
