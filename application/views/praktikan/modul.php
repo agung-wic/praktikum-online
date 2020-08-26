0f18457649a55bdef924e17cad293c305f3a90a1
dws16 <dauuuussss@gmail.com>
2020-08-26 19:36:37 +0700

    ganti warna hehe

--- application/views/praktikan/modul.php
+++ application/views/praktikan/modul.php
@@ -1,46 +1,52 @@
 <div class="container">
-  <div class="kotak mt-5">
-    <!-- Page Heading -->
-    <h1 class="mt-2 mb-5" style="text-align: center;"><?= $title ?></h1>
-    <div class="accordion" id="accordionExample">
-      <?php $i = 0;
-      foreach ($modul as $m) : ?>
-        <div class="card" style="background-color: #e2e1e6;">
-          <div class="card-header" id="heading<?= $m['modul']; ?>">
-            <h2 class="mb-0">
-              <button class="btn btn-light btn-block text-left collapsed" style="font-size: large;" type="button" data-toggle="collapse" data-target="#collapse<?= $m['modul']; ?>" aria-expanded="true" aria-controls="collapseOne">
-                <?= $m['modul'] ?>. <?= $m['name']; ?>
-              </button>
-            </h2>
-          </div>
+  <div class="row justify-content-center">
+    <div class="col-lg-10  align-self-center">
+      <div class="kotak mt-5 p-5">
+        <!-- Page Heading -->
+        <h1 class="mt-2 mb-5" style="text-align: center;"><?= $title ?></h1>
+        <div class="accordion" id="accordionExample">
+          <?php $i = 0;
+          foreach ($modul as $m) : ?>
+            <div class="card">
+              <div class="card-header py-3" style="margin-bottom: 0px; background: linear-gradient(40deg,rgba(111, 140, 252, 1) 8%,rgba(47, 200, 201, 1) 100%);color: white;" id="heading<?= $m['modul']; ?>">
+                <h2 class="mb-0">
+                  <div class="btn-block text-left collapsed" style="font-size: large; " type="button" data-toggle="collapse" data-target="#collapse<?= $m['modul']; ?>" aria-expanded="true">
+                    <div>
 
-          <div id="collapse<?= $m['modul']; ?>" class="collapse" aria-labelledby="heading<?= $m['modul']; ?>" data-parent="#accordionExample">
-            <div class="card-body">
-              Tujuan Percobaan: <?= $m['tujuan'] ?>
-              <p class="mt-4"><b>Waktu : <?= $m['time'] ?></b></p>
-              <p><b>Status : <?php
-                              if ($i < count($status)) {
-                                if ($m['modul'] == $status[$i]['modul_id']) {
-                                  if ($status[$i]['status'] == 0) {
-                                    echo "Belum Selesai";
+                    </div><strong><?= $m['modul'] ?></strong> . <?= $m['name']; ?>
+                  </div>
+                </h2>
+              </div>
+
+              <div id="collapse<?= $m['modul']; ?>" class="collapse" aria-labelledby="heading<?= $m['modul']; ?>" data-parent="#accordionExample">
+                <div class="card-body">
+                  Tujuan Percobaan: <?= $m['tujuan'] ?>
+                  <p class="mt-4"><b>Waktu : <?= $m['time'] ?></b></p>
+                  <p><b>Status : <?php
+                                  if ($i < count($status)) {
+                                    if ($m['modul'] == $status[$i]['modul_id']) {
+                                      if ($status[$i]['status'] == 0) {
+                                        echo "Belum Selesai";
+                                      } else {
+                                        echo "Selesai";
+                                      }
+                                      $i++;
+                                    } else {
+                                      echo "Belum Selesai";
+                                    }
                                   } else {
-                                    echo "Selesai";
+                                    echo "Belum Selesai";
                                   }
-                                  $i++;
-                                } else {
-                                  echo "Belum Selesai";
-                                }
-                              } else {
-                                echo "Belum Selesai";
-                              }
-                              ?></b></p>
-              <div class="row">
-                <a class="btn ml-auto gradien" href="<?= base_url() ?>praktikan/modul/<?= $m['modul']; ?>">Pilih Modul</a>
+                                  ?></b></p>
+                  <div class="row">
+                    <a class="btn ml-auto gradien" href="<?= base_url() ?>praktikan/modul/<?= $m['modul']; ?>">Pilih Modul</a>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
+          <?php endforeach; ?>
         </div>
-      <?php endforeach; ?>
+      </div>
     </div>
   </div>
 </div>
