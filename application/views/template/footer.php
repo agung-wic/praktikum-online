29382bc0f30c9736e583be9993607e555df742e0
mpuhambyah <mpu.hambyah@gmail.com>
2020-09-01 17:42:26 +0700

    coba server its

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -28,7 +28,7 @@
 
 <!-- Custom scripts for all pages-->
 <script src="<?= base_url(); ?>assets/js/sb-admin-2.min.js"></script>
-<script src="<?= base_url(); ?>assets/js/script.js?v=28"></script>
+<script src="<?= base_url(); ?>assets/js/script.js?v=29"></script>
 
 
 <script>
