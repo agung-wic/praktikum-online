371023c725da02d7a59b5517446d579d4e2fea59
dws16 <dauuuussss@gmail.com>
2020-09-05 14:02:20 +0700

    debug modal req jadwal

--- assets/js/script.js
+++ assets/js/script.js
@@ -155,6 +155,7 @@ $(function () {
 			method: "post",
 			dataType: "json",
 			success: function (data) {
+				console.log(data);
 				$("#name").val(data.name);
 				$("#nrp").val(data.nrp);
 				$("#modul_id").val(data.modul_id);
