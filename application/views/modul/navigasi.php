c4cfc0e53d97df3678f11de1ee85efe0bfdfed41
mpuhambyah <mpu.hambyah@gmail.com>
2020-09-16 03:03:11 +0700

    coba

--- application/views/modul/navigasi.php
+++ application/views/modul/navigasi.php
@@ -23,10 +23,10 @@
                             <button style="font-size: 300%;margin-top:3%;margin-bottom:20%" type="submit" class="btn btn-dark fa fa-arrow-circle-down tombolEditArah" data-id="<?= $tombol_arah[1]['id']; ?>" data-toggle="modal" data-target="#tombolEditArah">
                             </button>
                         </div>
-                        <?php $i = 2;
+                        <?php $i = 0;
                         foreach ($tombol_tulisan as $t) :  ?>
                             <div class="row justify-content-center mb-1">
-                                <button type="submit" style="margin: 1%;" data-toggle="modal" data-target="#tombolEditTulisan" data-id=" <?= $tombol_tulisan[$i]['id'] ?>" class="btn btn-dark px-4 tombolEditTulisan">
+                                <button type="submit" style="margin: 1%;" data-toggle="modal" data-target="#tombolEditTulisan" data-id=" <?= $tombol_tulisan[2]['id'] ?>" class="btn btn-dark px-4 tombolEditTulisan">
                                     <?= $t['tombol_keterangan'] ?><?php var_dump($tombol_tulisan[$i]['id']); ?>
                                 </button>
                             </div>
