49c220ea592e2cf6ffbcaba7955768d1c04fbefd
dws16 <dauuuussss@gmail.com>
2020-09-16 01:53:12 +0700

    ajax ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -347,7 +347,7 @@ $(function () {
 			method: "post",
 			dataType: "json",
 			success: function (data) {
-				$("#tombolEditArahModalLabel").html(data.tombol_keterangan);
+				$("#tombolEditArahLabel").html(data.tombol_keterangan);
 				$("id").val(data.id);
 				$("#tombol_kirim").val(data.tombol_kirim);
 				if (data.tombol_status == 1) {
