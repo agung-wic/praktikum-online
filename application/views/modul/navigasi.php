10a42fc06cb917f657347a2a21f03aa9c23bafd3
dws16 <dauuuussss@gmail.com>
2020-09-16 01:59:53 +0700

    ok

--- application/views/modul/navigasi.php
+++ application/views/modul/navigasi.php
@@ -75,7 +75,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                <button type="submit" class="btn btn-primary">Add</button>
+                <button type="submit" class="btn btn-primary">Ubah</button>
             </div>
             </form>
         </div>
