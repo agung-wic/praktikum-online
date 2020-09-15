e59a7850a6a937b8545959d9ad9f9b31b9ae2177
mpuhambyah <mpu.hambyah@gmail.com>
2020-09-16 03:49:11 +0700

    coba

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -27,7 +27,7 @@
 <script src="<?= base_url(); ?>assets/vendor/jquery-easing/jquery.easing.min.js"></script>
 
 <!-- Custom scripts for all pages-->
-<script src="<?= base_url(); ?>assets/js/sb-admin-2.min.js"></script>
+<script src="<?= base_url(); ?>assets/js/sb-admin-2.min.js?v=1"></script>
 <script src="<?= base_url(); ?>assets/js/script.js?v=67"></script>
 
 
