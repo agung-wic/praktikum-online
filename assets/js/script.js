1e2aa45a96b2b1209932d4f46f33e43d143afe21
mpuhambyah <mpu.hambyah@gmail.com>
2020-09-16 02:48:18 +0700

    coba

--- assets/js/script.js
+++ assets/js/script.js
@@ -368,6 +368,7 @@ $(function () {
 			method: "post",
 			dataType: "json",
 			success: function (data) {
+				console.log(data);
 				$("#tombolEditTulisanLabel").html(data.tombol_keterangan);
 				$("#id").val(data.id);
 				$("#tombol_keterangan").val(data.tombol_keterangan);
