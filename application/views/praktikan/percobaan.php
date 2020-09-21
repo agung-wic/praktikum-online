15b1912c454711b011ae077b27348bec01458ec7
mpuhambyah <mpu.hambyah@gmail.com>
2020-09-21 19:39:24 +0700

    coba

--- application/views/praktikan/percobaan.php
+++ application/views/praktikan/percobaan.php
@@ -30,7 +30,7 @@
         $i = 0;
         while ($i < count($live_stream)) {
         ?>
-          <a class="nav-item nav-link" id="nav-video-<?= $i ?>" data-toggle="tab" href="#video-<?= $i; ?>" role="tab" aria-controls="video-<?= $i; ?>" aria-selected="true">Video <?= $i; ?></a>
+          <a class="nav-item nav-link" style="background-color:white" id="nav-video-<?= $i ?>" data-toggle="tab" href="#video-<?= $i; ?>" role="tab" aria-controls="video-<?= $i; ?>" aria-selected="true">Video <?= $i; ?></a>
         <?php $i++;
         } ?>
       </div>
@@ -41,8 +41,10 @@
       while ($i < count($live_stream)) {
       ?>
         <div class="tab-pane fade" id="video-<?= $i; ?>" role="tabpanel" aria-labelledby="video-<?= $i; ?>-tab">
-          <div class="container text-center" style="transform:rotate(270deg);">
-            <iframe style="border-top-left-radius: 25px;border-top-right-radius: 25px;border-bottom-left-radius: 25px;border-bottom-right-radius: 25px;" width="650" height="650" src="<?= $live_stream[$i]['link'] ?>" frameborder="0" allow="accelerometer; mute; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <div class="kotak" style="background-color:black">
+            <div class="container text-center" style="transform:rotate(270deg);">
+              <iframe style="border-top-left-radius: 25px;border-top-right-radius: 25px;border-bottom-left-radius: 25px;border-bottom-right-radius: 25px;" width="650" height="650" src="<?= $live_stream[$i]['link'] ?>" frameborder="0" allow="accelerometer; mute; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
           </div>
         </div>
       <?php $i++;
