cac81e61ff9c0893e388268315f6f19dac2c2515
dws16 <dauuuussss@gmail.com>
2020-09-24 13:46:50 +0700

    debug

--- application/controllers/Praktikan.php
+++ application/controllers/Praktikan.php
@@ -7,6 +7,7 @@ class Praktikan extends CI_Controller
     {
         parent::__construct();
         is_logged_in();
+        $this->socket = NULL;
     }
 
     public function index()
@@ -67,7 +68,7 @@ class Praktikan extends CI_Controller
         $data['lokasi'] = ($this->uri->segment('3') == NULL) ? 'tes' : $this->uri->segment('3');
         if ((time() >= $jadwal) && ((time() <= ($jadwal + $time)))) {
             if ($cek['status'] == 0) {
-                $this->_connectsocket($id);
+                $this->socket = $this->_connectsocket($id);
                 $data['output'] = NULL;
 
                 $data['title'] = 'Percobaan Praktikum';
@@ -253,8 +254,8 @@ class Praktikan extends CI_Controller
 
     public function getpercobaan()
     {
-        $connect = $this->_connectsocket($this->input->post('id'));
-        $hasil = $this->_sendsocket($connect[0][0], $connect[0][1], $this->input->post('kirim'), $this->input->post('id'));
+        //$connect = $this->_connectsocket($this->input->post('id'));
+        $hasil = $this->socket;
         if ($hasil) {
             echo json_encode($hasil);
         } else {
