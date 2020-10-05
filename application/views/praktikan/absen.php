ead97659c42f37b04311d7795c4392eba047baf4
dws16 <dauuuussss@gmail.com>
2020-10-05 16:07:10 +0700

    front end absen

--- application/views/praktikan/absen.php
+++ application/views/praktikan/absen.php
@@ -1,15 +1,32 @@
-<div class="row mx-5">
-    <div class="col-lg-6 justify-content-center">
-        <div class="card border-left-warning h-100 py-2" style="background-color: white;">
+<div class="row m-5 justify-content-center">
+    <div class="col-8 justify-content-center">
+        <div class="card border-bottom-danger h-100 py-2" style="background-color: white;">
             <div class="card-header">
-                <h6>Selamat Datang</h6>
+                <h6>Presensi Praktikum</h6>
             </div>
             <div class="card-body">
-                <h4><b><?= $user['name'] ?></b></h4>
-                <h5><b><?= $user['nrp'] ?></b></h5>
+                <div class="row">
+                    <div class="col">
+                        <h4 class="text-center"><b><?= $modul['name'] ?></b></h4>
+                        <h5 class="text-center"><b><?= $status['jadwal'] ?></b></h5>
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col">
+                        <h6 class="text-center"><?= $user['name'] ?></h6>
+                        <h6 class="text-center"><?= $user['nrp'] ?></h6>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer">
+                <form action="<?= base_url('praktikan/do_absen/') ?>" method="post">
+                    <input type="text" name="nrp" value="<?= $user['nrp']; ?>" hidden>
+                    <input type="text" name="modul_id" value="<?= $modul['modul']; ?>" hidden>
+                    <div class="row justify-content-center">
+                        <button class="btn gradien px-5" type="submit"> Hadir </button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 </div>
-<p class="card-text">Praktikum :<?= ' ' . $modul['name'] ?></p>
-<p class="card-text">Jadwal :<?= ' ' . $status['jadwal'] ?></p>
