d668fc2e707b86cba645a6fd3fb14562448ab7cf
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-06 13:49:23 +0700

    coba

--- application/controllers/Praktikan.php
+++ application/controllers/Praktikan.php
@@ -319,6 +319,7 @@ class Praktikan extends CI_Controller
         $data['tombol_arah'] = $this->db->get_where('tombol_arah', ['id_modul' => $id])->result_array();
         $data['tombol_tulisan'] = $this->db->get_where('tombol_tulisan', ['id_modul' => $id])->result_array();
         $data['output_tulisan'] = $this->db->get_where('output_tulisan', ['id_modul' => $id])->result_array();
+        var_dump($id);
         var_dump($data['tombol_arah']);
         die;
         if (socket_write($socket1, $message, strlen($message))) {
