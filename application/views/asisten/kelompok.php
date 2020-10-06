f1a8c70bdac58667376f3bdeb050a5dfb9f58321
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-06 19:49:24 +0700

    coba

--- application/views/asisten/kelompok.php
+++ application/views/asisten/kelompok.php
@@ -8,10 +8,24 @@
         <!-- DataTales Example -->
         <div class="kotak">
             <div class="card-header">
+                <div class="row">
+                    <div class="col">
+                        <h6 class="m-0 font-weight-bold "><?= $title; ?></h6>
+                    </div>
+                    <div class="col-auto mr-auto">
+                        <a href="" class="btn gradien mb-3 tampilTambahJadwal" data-toggle="modal" data-target="#JadwalEdit">Tambah Jadwal Baru</a>
+                        <a href="" class="btn gradien mb-3" data-toggle="modal" data-target="#JadwalAddFile"><i class="fas fa-file-csv"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="card-body">
 
+                </div>
             </div>
+
         </div>
         <!-- /.container-fluid -->
+
     </div>
     <!-- End of Main Content -->
 </div>
