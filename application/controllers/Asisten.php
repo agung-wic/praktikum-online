172660feb3d0c9547bf4c37c370de3c236105127
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-06 20:35:46 +0700

    coba

--- application/controllers/Asisten.php
+++ application/controllers/Asisten.php
@@ -51,6 +51,8 @@ class Asisten extends CI_Controller
     $data['user'] = $this->db->get_where('user', ['nrp' => $this->session->userdata('nrp')])->row_array();
     $data['title'] = 'Pembagian Kelompok';
     $data['kelompok'] = $this->db->get_where('kelompok', ['no_kelompok' => $id])->row_array();
+    var_dump($data['kelompok']);
+    die;
     $this->load->view('template/header', $data);
     $this->load->view('template/sidebar', $data);
     $this->load->view('template/topbar', $data);
