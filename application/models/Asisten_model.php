ecf7780687f925ff1bea21e149199adebec5a226
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-08 02:56:06 +0700

    coba

--- application/models/Asisten_model.php
+++ application/models/Asisten_model.php
@@ -145,6 +145,20 @@ class Asisten_model extends CI_Model
     return $this->db->query($query)->result_array();
   }
 
+  public function KelompokAsisten($id)
+  {
+    $query = "SELECT COUNT(`anggota_kelompok`.`nrp`) AS jumlah , `kelompok_asisten`.`id` , `kelompok_asisten`.`nrp` as `nrp` , `kelompok_asisten`.`id_modul` as `id_modul` , `kelompok_asisten`.`no_kelompok` as `no_kelompok` ,
+              `user`.`name`,`kelompok`.`no_kelompok` FROM `kelompok_asisten` 
+              INNER JOIN `user` ON `user`.`nrp` = `kelompok_asisten`.`nrp` 
+              INNER JOIN `kelompok` ON `kelompok`.`id` = `kelompok_asisten`.`no_kelompok` 
+              INNER JOIN `modul` ON `modul`.`modul` = `kelompok_asisten`.`id_modul` 
+              INNER JOIN `anggota_kelompok` ON `anggota_kelompok` . `no_kelompok` = `kelompok_asisten` . `no_kelompok`
+              WHERE `kelompok_asisten`.`nrp` = $id";
+
+    return $this->db->query($query)->result_array();
+  }
+
+
   public function Tampildetailkelompok($id)
   {
     $query = "SELECT `anggota_kelompok`.`id` , `anggota_kelompok`.`nrp` as `nrp` , `user`.`name` as 'name' , `anggota_kelompok`.`no_kelompok` FROM `user` 
