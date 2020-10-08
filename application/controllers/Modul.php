be6df73717a83b7b9edec709a889ea27abb2b17e
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-09 04:24:05 +0700

    ini

--- application/controllers/Modul.php
+++ application/controllers/Modul.php
@@ -47,7 +47,7 @@ class Modul extends CI_Controller
 
         $config['attributes'] = array('class' => 'page-link');
 
-        $data['start'] = $this->uri->segment(3);
+        $data['start'] = $this->uri->segment(4);
         if ($data['start'] == null) {
             $data['start'] = 0;
         }
