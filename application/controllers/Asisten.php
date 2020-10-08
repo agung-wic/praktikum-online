fe37c906e9c0cbebcbc8c34f7c9ed7ba6d5d630e
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-08 20:39:22 +0700

    list absen

--- application/controllers/Asisten.php
+++ application/controllers/Asisten.php
@@ -236,6 +236,31 @@ class Asisten extends CI_Controller
     redirect(base_url('asisten'));
   }
 
+  public function absen()
+  {
+    $this->load->model('Asisten_model');
+    $data['modul'] = $this->db->get('modul')->result_array();
+    $data['user'] = $this->db->get_where('user', ['nrp' => $this->session->userdata('nrp')])->row_array();
+    $id_kelompok = $this->db->get_where('anggota_kelompok', ['nrp ' => $this->session->userdata('nrp')])->row_array();
+    $data['kelompok'] = $this->db->get_where('kelompok', ['id' => $id_kelompok['no_kelompok']])->row_array();
+    $data['status'] = $this->db->get('jadwal')->result_array();
+    $data['title'] = 'List Absen';
+    if (!$id) {
+      $this->load->view('template/header', $data);
+      $this->load->view('template/sidebar', $data);
+      $this->load->view('template/topbar', $data);
+      $this->load->view('asisten/absen', $data);
+      $this->load->view('template/footer');
+    } else {
+      $data['kelompok'] = $this->Asisten_model->JumlahKelompok();
+      $this->load->view('template/header', $data);
+      $this->load->view('template/sidebar', $data);
+      $this->load->view('template/topbar', $data);
+      $this->load->view('asisten/absenkelompok', $data);
+      $this->load->view('template/footer');
+    }
+  }
+
   public function absen($id = NULL)
   {
     $data['modul'] = $this->db->get('modul')->result_array();
@@ -244,10 +269,18 @@ class Asisten extends CI_Controller
     $data['kelompok'] = $this->db->get_where('kelompok', ['id' => $id_kelompok['no_kelompok']])->row_array();
     $data['status'] = $this->db->get('jadwal')->result_array();
     $data['title'] = 'List Absen';
-    $this->load->view('template/header', $data);
-    $this->load->view('template/sidebar', $data);
-    $this->load->view('template/topbar', $data);
-    $this->load->view('asisten/absen', $data);
-    $this->load->view('template/footer');
+    if (!$id) {
+      $this->load->view('template/header', $data);
+      $this->load->view('template/sidebar', $data);
+      $this->load->view('template/topbar', $data);
+      $this->load->view('asisten/absen', $data);
+      $this->load->view('template/footer');
+    } else {
+      $this->load->view('template/header', $data);
+      $this->load->view('template/sidebar', $data);
+      $this->load->view('template/topbar', $data);
+      $this->load->view('asisten/absenkelompok', $data);
+      $this->load->view('template/footer');
+    }
   }
 }
