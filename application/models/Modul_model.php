e7400427da452a4d3b2f25b0ff9c287d37d476ec
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-09 05:34:59 +0700

    nilai

--- application/models/Modul_model.php
+++ application/models/Modul_model.php
@@ -20,7 +20,7 @@ class Modul_model extends CI_Model
 
         return $this->db->query($query)->num_rows();
     }
-    
+
 
     public function TampilUser($limit, $start, $keyword = null, $no_kelompok)
     {
@@ -50,139 +50,137 @@ class Modul_model extends CI_Model
         return $this->db->query($query)->result_array();
     }
 
-    
 
 
-      
-      
-        public function TampilJadwal($limit, $start, $keyword = null)
-        {
-          $keyword = $this->input->post('keyword1', true);
-          if ($keyword) {
+
+
+
+    public function TampilJadwal($limit, $start, $keyword = null)
+    {
+        $keyword = $this->input->post('keyword1', true);
+        if ($keyword) {
             $query = "SELECT `jadwal`.`id`,  `user`.`nrp`, `user`.`name` as `name`, `modul`.`name` as `modul`, `jadwal`.`jadwal`
                     FROM `user` INNER JOIN `jadwal` ON `user`.`nrp` = `jadwal`.`nrp` 
                     INNER JOIN `modul` ON `modul`.`modul` = `jadwal`.`modul_id`
                     WHERE `user`.`name` LIKE '%$keyword%'
                     OR `modul`.`name` LIKE '%$keyword%'
                     OR `jadwal`.`jadwal` LIKE '%$keyword%' ";
-          } else {
+        } else {
             $query = "SELECT `jadwal`.`id`, `user`.`name` as 'name', `user`.`nrp`, `modul`.`modul` as 'modul_id', `modul`.`name` as 'modul', `jadwal`.`jadwal`
                     FROM `user` INNER JOIN `jadwal` ON `user`.`nrp` = `jadwal`.`nrp` 
                     INNER JOIN `modul` ON `modul`.`modul` = `jadwal`.`modul_id` LIMIT $limit OFFSET $start";
-          }
-          return $this->db->query($query)->result_array();
         }
-      
-        public function JumlahJadwal()
-        {
-          $query = "SELECT `jadwal`.`id`, `user`.`name` as 'name', `user`.`nrp`, `modul`.`modul` as 'modul_id', `modul`.`name` as 'modul', `jadwal`.`jadwal`
+        return $this->db->query($query)->result_array();
+    }
+
+    public function JumlahJadwal()
+    {
+        $query = "SELECT `jadwal`.`id`, `user`.`name` as 'name', `user`.`nrp`, `modul`.`modul` as 'modul_id', `modul`.`name` as 'modul', `jadwal`.`jadwal`
                     FROM `user` INNER JOIN `jadwal` ON `user`.`nrp` = `jadwal`.`nrp` 
                     INNER JOIN `modul` ON `modul`.`modul` = `jadwal`.`modul_id`";
-      
-          return $this->db->query($query)->num_rows();
-        }
-      
-        public function TampilPengumuman($limit, $start)
-        {
-          $query = "SELECT `pengumuman`.`id`,`pengumuman`.`judul`, `user`.`name` as 'name', `user`.`nrp`, `pengumuman`.`isi`, `pengumuman`.`tanggal`
+
+        return $this->db->query($query)->num_rows();
+    }
+
+    public function TampilPengumuman($limit, $start)
+    {
+        $query = "SELECT `pengumuman`.`id`,`pengumuman`.`judul`, `user`.`name` as 'name', `user`.`nrp`, `pengumuman`.`isi`, `pengumuman`.`tanggal`
                     FROM `user` INNER JOIN `pengumuman` ON `user`.`nrp` = `pengumuman`.`nrp`LIMIT $limit OFFSET $start";
-      
-          return $this->db->query($query)->result_array();
-        }
-      
-        public function JumlahPengumuman()
-        {
-          $query = "SELECT `pengumuman`.`id`,`pengumuman`.`judul`, `user`.`name` as 'name', `user`.`nrp`, `pengumuman`.`isi`, `pengumuman`.`tanggal`
+
+        return $this->db->query($query)->result_array();
+    }
+
+    public function JumlahPengumuman()
+    {
+        $query = "SELECT `pengumuman`.`id`,`pengumuman`.`judul`, `user`.`name` as 'name', `user`.`nrp`, `pengumuman`.`isi`, `pengumuman`.`tanggal`
                     FROM `user` INNER JOIN `pengumuman` ON `user`.`nrp` = `pengumuman`.`nrp`";
-      
-          return $this->db->query($query)->num_rows();
-        }
-      
-        public function EditPengumuman()
-        {
-          $id = $this->input->post('id', true);
-          $query = "SELECT `pengumuman`.`id`,`pengumuman`.`judul`, `user`.`name` as 'name', `user`.`nrp`, `pengumuman`.`isi`, `pengumuman`.`tanggal`
+
+        return $this->db->query($query)->num_rows();
+    }
+
+    public function EditPengumuman()
+    {
+        $id = $this->input->post('id', true);
+        $query = "SELECT `pengumuman`.`id`,`pengumuman`.`judul`, `user`.`name` as 'name', `user`.`nrp`, `pengumuman`.`isi`, `pengumuman`.`tanggal`
                     FROM `user` INNER JOIN `pengumuman` ON `user`.`nrp` = `pengumuman`.`nrp` WHERE `pengumuman`.`id`='$id'";
-      
-          return $this->db->query($query)->row_array();
-        }
-      
-        public function CariJadwal()
-        {
-          $keyword = $this->input->post('keyword1', true);
-      
-          $query = "SELECT `jadwal`.`id`,  `user`.`nrp`, `user`.`name` as `name`, `modul`.`name` as `modul`, `jadwal`.`jadwal`
+
+        return $this->db->query($query)->row_array();
+    }
+
+    public function CariJadwal()
+    {
+        $keyword = $this->input->post('keyword1', true);
+
+        $query = "SELECT `jadwal`.`id`,  `user`.`nrp`, `user`.`name` as `name`, `modul`.`name` as `modul`, `jadwal`.`jadwal`
                     FROM `user` INNER JOIN `jadwal` ON `user`.`nrp` = `jadwal`.`nrp` 
                     INNER JOIN `modul` ON `modul`.`modul` = `jadwal`.`modul_id`
                     WHERE `user`.`name` LIKE '%$keyword%'
                     OR `modul`.`name` LIKE '%$keyword%'
                     OR `jadwal`.`jadwal` LIKE '%$keyword%'";
-      
-          return $this->db->query($query)->result_array();
-        }
-      
-        public function TampilReqJadwal()
-        {
-          $query = "SELECT `req_jadwal`.`id`, `user`.`name` as 'name', `user`.`nrp`, `modul`.`modul` as 'modul_id', `modul`.`name` as 'modul', `req_jadwal`.`jadwal_old`
+
+        return $this->db->query($query)->result_array();
+    }
+
+    public function TampilReqJadwal()
+    {
+        $query = "SELECT `req_jadwal`.`id`, `user`.`name` as 'name', `user`.`nrp`, `modul`.`modul` as 'modul_id', `modul`.`name` as 'modul', `req_jadwal`.`jadwal_old`
                     FROM `user` INNER JOIN `req_jadwal` ON `user`.`nrp` = `req_jadwal`.`nrp` 
                     INNER JOIN `modul` ON `modul`.`modul` = `req_jadwal`.`modul_id` WHERE `req_jadwal`.`is_approved`=0";
-      
-          return $this->db->query($query)->result_array();
-        }
-      
-        public function DetailJadwal($id)
-        {
-          $query = "SELECT `req_jadwal`.`id`, `user`.`name` as 'name', `user`.`nrp`, `modul`.`modul` as 'modul_id', 
+
+        return $this->db->query($query)->result_array();
+    }
+
+    public function DetailJadwal($id)
+    {
+        $query = "SELECT `req_jadwal`.`id`, `user`.`name` as 'name', `user`.`nrp`, `modul`.`modul` as 'modul_id', 
                     `modul`.`name` as 'modul', `req_jadwal`.`jadwal_old`, `req_jadwal`.`jadwal_new`, `req_jadwal`.`ket` 
                     FROM `user` INNER JOIN `req_jadwal` ON `user`.`nrp` = `req_jadwal`.`nrp` 
                     INNER JOIN `modul` ON `modul`.`modul` = `req_jadwal`.`modul_id` WHERE `req_jadwal`.`id` = '$id'
                     ";
-      
-          return $this->db->query($query)->row_array();
-        }
-      
-        public function CariReqJadwal()
-        {
-          $keyword = $this->input->post('keyword2', true);
-      
-          $query = "SELECT `req_jadwal`.`id`, `user`.`nrp`, `user`.`name` as `name`, `modul`.`name` as `modul`, `req_jadwal`.`jadwal_old`
+
+        return $this->db->query($query)->row_array();
+    }
+
+    public function CariReqJadwal()
+    {
+        $keyword = $this->input->post('keyword2', true);
+
+        $query = "SELECT `req_jadwal`.`id`, `user`.`nrp`, `user`.`name` as `name`, `modul`.`name` as `modul`, `req_jadwal`.`jadwal_old`
                     FROM `user` INNER JOIN `req_jadwal` ON `user`.`nrp` = `req_jadwal`.`nrp` 
                     INNER JOIN `modul` ON `modul`.`modul` = `req_jadwal`.`modul_id`
                     WHERE `req_jadwal`.`is_approved`=0 AND (`user`.`name` LIKE '%$keyword%'
                     OR `modul`.`name` LIKE '%$keyword%'
                     OR `req_jadwal`.`nrp` LIKE '%$keyword%'
                     OR `req_jadwal`.`jadwal_old` LIKE '%$keyword%')";
-      
-          return $this->db->query($query)->result_array();
-        }
-      
-        public function TampilJadwalPraktikan()
-        {
-          $id = $this->input->post('id', true);
-          $query = "SELECT `jadwal`.`id`, `user`.`name` as 'name',`user`.`nrp`, `modul`.`modul` as 'modul_id', 
+
+        return $this->db->query($query)->result_array();
+    }
+
+    public function TampilJadwalPraktikan()
+    {
+        $id = $this->input->post('id', true);
+        $query = "SELECT `jadwal`.`id`, `user`.`name` as 'name',`user`.`nrp`, `modul`.`modul` as 'modul_id', 
                     `modul`.`name` as 'modul', `jadwal`.`jadwal` FROM `user` 
                     INNER JOIN `jadwal` ON `user`.`nrp` = `jadwal`.`nrp` 
                     INNER JOIN `modul` ON `modul`.`modul` = `jadwal`.`modul_id` 
                     WHERE `jadwal`.`id`='$id'";
-      
-          return $this->db->query($query)->row_array();
-        }
-      
-        public function TampilUserOnline($limit, $start)
-        {
-          $query = "SELECT * FROM `user` 
+
+        return $this->db->query($query)->row_array();
+    }
+
+    public function TampilUserOnline($limit, $start)
+    {
+        $query = "SELECT * FROM `user` 
                     WHERE `is_online`= 1 AND `role_id`= 2 LIMIT $limit OFFSET $start ";
-      
-          return $this->db->query($query)->result_array();
-        }
-      
-        public function JumlahUserOnline()
-        {
-          $query = "SELECT * FROM `user` 
+
+        return $this->db->query($query)->result_array();
+    }
+
+    public function JumlahUserOnline()
+    {
+        $query = "SELECT * FROM `user` 
                     WHERE `is_online`= 1 AND `role_id`= 2";
-      
-          return $this->db->query($query)->num_rows();
-        }
-      }
-      
+
+        return $this->db->query($query)->num_rows();
+    }
 }
