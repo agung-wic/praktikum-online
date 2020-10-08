1142e5055b59fd2f98bb19c77030f0875e51b4bc
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-08 20:49:40 +0700

    list absen

--- application/views/asisten/absen.php
+++ application/views/asisten/absen.php
@@ -8,7 +8,7 @@
                 $a = 0;
                 foreach ($modul as $m) : ?>
                     <div class="card pb-2">
-                        <a style="border: none;" href="<?= base_url() ?>asisten/absen/<?= $m['modul']; ?>" class="card-header gradien_modul py-3">
+                        <a style="border: none;text-decoration: none;" href="<?= base_url() ?>asisten/absen/<?= $m['modul']; ?>" class="card-header gradien_modul py-3">
                             <h2 class="mb-0">
                                 <div class="btn-block text-left collapsed" style="font-size: large; " type="button">
                                     <strong><?= $m['modul'] ?></strong> . <?= $m['name']; ?>
