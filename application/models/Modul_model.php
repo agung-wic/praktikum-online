4b6aaa12fc05a2bcf75aaaa5ca49a4631a4953a7
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-09 16:56:40 +0700

    jadwal kelompok

--- application/models/Modul_model.php
+++ application/models/Modul_model.php
@@ -190,10 +190,11 @@ class Modul_model extends CI_Model
     public function TampilJadwalPraktikan()
     {
         $id = $this->input->post('id', true);
-        $query = "SELECT `jadwal`.`id`, `user`.`name` as 'name',`user`.`nrp`, `modul`.`modul` as 'modul_id', 
+        $query = "SELECT `jadwal`.`id`, `user`.`name` as 'name',`user`.`nrp`, `modul`.`modul` as 'modul_id', `anggota_kelompok`.`no_kelompok` as 'no_kelompok'
                     `modul`.`name` as 'modul', `jadwal`.`jadwal` FROM `user` 
                     INNER JOIN `jadwal` ON `user`.`nrp` = `jadwal`.`nrp` 
                     INNER JOIN `modul` ON `modul`.`modul` = `jadwal`.`modul_id` 
+                    LEFT JOIN `anggota_kelompok` ON `user`.`nrp` = `anggota_kelompok`.`nrp` 
                     WHERE `jadwal`.`id`='$id'";
 
         return $this->db->query($query)->row_array();
