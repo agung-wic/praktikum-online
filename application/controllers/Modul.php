37c8ccde09ec482ffc286935bf4a12fd6404d379
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-09 11:23:01 +0700

    pindah controller admin

--- application/controllers/Modul.php
+++ application/controllers/Modul.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Praktikum extends CI_Controller
+class Modul extends CI_Controller
 {
     public function __construct()
     {
@@ -18,7 +18,7 @@ class Praktikum extends CI_Controller
         $this->load->view('template/header', $data);
         $this->load->view('template/sidebar', $data);
         $this->load->view('template/topbar', $data);
-        $this->load->view('praktikum/index', $data);
+        $this->load->view('modul/index', $data);
         $this->load->view('template/footer');
     }
 
