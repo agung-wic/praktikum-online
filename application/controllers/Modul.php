a70b411ae5d2bb161adc49d514d9da5a78846a1a
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-09 12:46:50 +0700

    manajemen asisten

--- application/controllers/Modul.php
+++ application/controllers/Modul.php
@@ -678,7 +678,34 @@ class Modul extends CI_Controller
         $this->load->view('template/footer');
     }
 
+    public function manajemenasisten()
+    {
+        $this->load->model('Modul_model');
+        $data['user'] = $this->db->get_where('user', ['nrp' => $this->session->userdata('nrp')])->row_array();
+        $data['title'] = 'Pembagian Kelompok';
+        $data['kelompok'] = $this->Modul_model->JumlahKelompok();
+        $this->load->view('template/header', $data);
+        $this->load->view('template/sidebar', $data);
+        $this->load->view('template/topbar', $data);
+        $this->load->view('modul/kelompok', $data);
+        $this->load->view('template/footer');
+    }
+
     public function detailkelompok($id)
+    {
+        $this->load->model('Modul_model');
+        $data['user'] = $this->db->get_where('user', ['nrp' => $this->session->userdata('nrp')])->row_array();
+        $data['title'] = 'Pembagian Kelompok';
+        $data['kelompok'] = $this->Modul_model->Tampildetailkelompok($id);
+        $data['id_kelompok'] = $id;
+        $this->load->view('template/header', $data);
+        $this->load->view('template/sidebar', $data);
+        $this->load->view('template/topbar', $data);
+        $this->load->view('modul/detailkelompok', $data);
+        $this->load->view('template/footer');
+    }
+
+    public function detailmanajemenasisten($id)
     {
         $this->load->model('Modul_model');
         $data['user'] = $this->db->get_where('user', ['nrp' => $this->session->userdata('nrp')])->row_array();
