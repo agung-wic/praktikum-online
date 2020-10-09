4a55fe53b3066213a2471d0f4195ffea293b8958
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-09 13:02:32 +0700

    manajemen asisten

--- application/models/Modul_model.php
+++ application/models/Modul_model.php
@@ -71,7 +71,7 @@ class Modul_model extends CI_Model
         return $this->db->query($query)->result_array();
     }
 
-    public function Tampildetailkelompok($id)
+    public function TampildetailkelompokAsisten($id)
     {
         $query = "SELECT `kelompok_asisten`.`id` , `kelompok_asisten`.`nrp` as `nrp` , `user`.`name` as 'name' , `kelompok_asisten`.`no_kelompok` FROM `kelompok_asisten` 
               INNER JOIN `user` ON `kelompok_asisten`.`nrp` = `user`.`nrp` 
