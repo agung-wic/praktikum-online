b634a7d7214d308f3e5c752811d6ceb6d39881a6
dws16 <dauuuussss@gmail.com>
2020-10-09 17:01:10 +0700

    ok

--- application/views/praktikan/modul.php
+++ application/views/praktikan/modul.php
@@ -51,7 +51,7 @@
                   if ($m['modul'] == $asisten[$j]['id_modul']) {
 
                     $halo = 0;
-                    echo "<p style='margin-bottom:0px;'>Asisten : " . $m['modul'] . " = " . $asisten[$j]['id_modul'] . " XX " . $j . "</p>";
+                    echo "<p style='margin-bottom:0px;'>Asisten : </p>";
                     while ($halo < count($flag)) {
 
                   ?> <li><b><?= $asisten[$flag[$halo]]['name'] ?> - <?= $asisten[$flag[$halo]]['nrp'] ?></b></li>
@@ -59,8 +59,6 @@
                   <?php $halo++;
                       $j++;
                     }
-                  } else {
-                    echo "<p style='margin-bottom:0px;'>Asisten : " . $m['modul'] . " = " . $asisten[$j]['id_modul'] . " XX " . $j . "</p>";
                   }
                   ?>
                   <div class="row">
