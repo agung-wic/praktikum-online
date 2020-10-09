5bc10403bd183086fa9d44cea1d32ee334e55a46
dws16 <dauuuussss@gmail.com>
2020-10-10 01:00:12 +0700

    tes

--- application/controllers/Modul.php
+++ application/controllers/Modul.php
@@ -396,6 +396,8 @@ class Modul extends CI_Controller
                 $csv = fgetcsv($data, 0, ';');
                 $id_kelompok = $this->db->get_where('kelompok', ['no_kelompok' => $csv[0]])->row_array();
                 $anggota = $this->db->get_where('anggota_kelompok', ['no_kelompok' => $id_kelompok['id']])->result_array();
+                var_dump($anggota);
+                die;
                 foreach ($anggota as $a) {
                     $jadwal = [
                         "nrp" => $a['nrp'],
