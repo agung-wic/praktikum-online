72910a5e91a4a156f81723e2822fa8dd4ac08c87
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-15 00:27:52 +0700

    nilai akhir

--- application/controllers/Asisten.php
+++ application/controllers/Asisten.php
@@ -250,6 +250,9 @@ class Asisten extends CI_Controller
       'kesimpulan' => $this->input->post('kesimpulan', true),
       'asisten' => $this->session->userdata('nrp')
     ];
+    $jumlah = $data['resume'] + $data['pretest'];
+    var_dump($jumlah);
+    die;
     $this->db->where('id', $this->input->post('id'));
     $this->db->update('nilai', $data);
     $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
