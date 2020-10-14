ffe0db9d4bc68b154f78db6fa71011f85a71745b
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-15 02:13:43 +0700

    login asisten

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -168,19 +168,23 @@ class Auth extends CI_Controller
                 $data = [
                     'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
                     'role_id' => 7,
-                    'is_active' => 1,
+                    'is_active' => 0,
                     'date_created' => time()
                 ];
                 $this->db->where('nrp', $nrp);
                 $this->db->update('user', $data);
 
+                $token = base64_encode(random_bytes(32));
                 $user_token = [
+                    'token' => $token,
                     'date_created' => time()
                 ];
 
                 $user = $this->db->get_where('user', ['nrp' => $nrp])->row_array();
 
                 $this->db->insert('user_token', $user_token);
+
+                $this->_sendEmail($token, 'verify', $user);
 
                 $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
                                             Selamat! Akun berhasil dibuat. Silakan periksa email untuk aktivasi!
