e5e83c9afb1dccfb26371e3f5849da406579b22a
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-15 01:12:14 +0700

    nilai akhir

--- application/controllers/Asisten.php
+++ application/controllers/Asisten.php
@@ -251,19 +251,19 @@ class Asisten extends CI_Controller
       'asisten' => $this->session->userdata('nrp')
     ];
     $nilai_angka = (($data['resume'] * 0.25) + ($data['pretest'] * 0.05) + ($data['uji_lisan'] * 0.1) + ($data['praktikum'] * 0.1) + ($data['postest'] * 0.05) + ($data['format'] * 0.1) + ($data['bab'] * 0.25) + ($data['kesimpulan'] * 0.1));
-    if ($nilai_angka >= 86 && $nilai_angka <= 100) {
+    if ($nilai_angka >= 86.00 && $nilai_angka <= 100.00) {
       $nilai_angka_abjad = "A";
-    } else if ($nilai_angka >= 76 && $nilai_angka <= 85) {
+    } else if ($nilai_angka >= 76.00 && $nilai_angka <= 85.00) {
       $nilai_angka_abjad = "AB";
-    } else if ($nilai_angka >= 66 && $nilai_angka <= 75) {
+    } else if ($nilai_angka >= 66.00 && $nilai_angka <= 75.00) {
       $nilai_angka_abjad = "B";
-    } else if ($nilai_angka >= 61 && $nilai_angka <= 65) {
+    } else if ($nilai_angka >= 61.00 && $nilai_angka <= 65.00) {
       $nilai_angka_abjad = "BC";
-    } else if ($nilai_angka >= 56 && $nilai_angka <= 60) {
+    } else if ($nilai_angka >= 56.00 && $nilai_angka <= 60.00) {
       $nilai_angka_abjad = "C";
-    } else if ($nilai_angka >= 41 && $nilai_angka <= 55) {
+    } else if ($nilai_angka >= 41.00 && $nilai_angka <= 55.00) {
       $nilai_angka_abjad = "D";
-    } else if ($nilai_angka >= 0 && $nilai_angka <= 40) {
+    } else if ($nilai_angka >= 0.00 && $nilai_angka <= 40.00) {
       $nilai_angka_abjad = "E";
     }
     $data['nilai_angka'] = $nilai_angka;
