dcffa53e3190a92cad910d5c14f1658e2f3983dd
dws16 <dauuuussss@gmail.com>
2020-10-14 22:14:20 +0700

    ok

--- application/controllers/Modul.php
+++ application/controllers/Modul.php
@@ -528,9 +528,9 @@ class Modul extends CI_Controller
             while (!feof($data)) {
                 $csv = fgetcsv($data, 0, ';');
                 $nrp = mb_convert_encoding($csv[0], "ISO-8859-1", "UTF-8");
-                $no_kelompok = mb_convert_encoding($csv[1], "ISO-8859-1", "UTF-8");
+                $no_kelompok = mb_convert_encoding($csv[2], "ISO-8859-1", "UTF-8");
                 $id_kelompok = $this->db->get_where('kelompok', ['no_kelompok' => $no_kelompok])->row_array();
-                $id_modul = mb_convert_encoding($csv[2], "ISO-8859-1", "UTF-8");
+                $id_modul = mb_convert_encoding($csv[1], "ISO-8859-1", "UTF-8");
                 $cek = $this->db->get_where('kelompok_asisten', ['nrp' => $nrp, 'id_modul' => $id_modul, 'no_kelompok' => $no_kelompok])->row_array();
                 if ($cek) {
                     $this->session->set_flashdata('message1', '<div class="alert alert-danger" role="alert">
