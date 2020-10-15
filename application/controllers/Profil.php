becc216d72dcb52d9564a8f86d5714d727d8e02c
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-15 10:17:56 +0700

    profil role

--- application/controllers/Profil.php
+++ application/controllers/Profil.php
@@ -13,6 +13,8 @@ class Profil extends CI_Controller
   {
     $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
     $data['role_id'] = $this->db->get_where('user_role', ['id' => $data['user']])->row_array();
+    var_dump($data['role_id']);
+    die;
     $data['title'] = 'Profil Saya';
     $this->load->view('template/header', $data);
     $this->load->view('template/sidebar', $data);
