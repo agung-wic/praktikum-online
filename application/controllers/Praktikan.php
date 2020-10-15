bd612aa16fcd084d3312a54d70dc034b86c542f6
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-15 11:26:48 +0700

    profil role

--- application/controllers/Praktikan.php
+++ application/controllers/Praktikan.php
@@ -355,15 +355,15 @@ class Praktikan extends CI_Controller
                 $result2 = str_replace("]", "", $result2);
                 $result2 = explode(",", $result2);
 
-                if (count($result2) > 1) {
-                    if ($compare[1] == "1" || $compare[1] == "0") {
-                        $result2 = $result2[0] . " " . $satuan;
-                    } else {
-                        $result2 = $result2[0] . " " . $satuan;
-                    }
-                } else {
-                    $result2 = $satuan;
-                }
+                // if (count($result2) > 1) {
+                //     if ($compare[1] == "1" || $compare[1] == "0") {
+                //         $result2 = $result2[0] . " " . $satuan;
+                //     } else {
+                //         $result2 = $result2[0] . " " . $satuan;
+                //     }
+                // } else {
+                //     $result2 = $satuan;
+                // }
                 return $result2;
             } else {
                 return "Error 1";
