a3ba257c6ac90fa24cde22a7291bc5ff87e9290a
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-24 10:45:10 +0700

    verify error

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -255,7 +255,7 @@ class Auth extends CI_Controller
         $config = [
             'protocol' => 'smtp',
             'smtp_host' => 'ssl://smtp.googlemail.com',
-            'smtp_user' => 'fisikadasar001@gmail.com',
+            'smtp_user' => 'fisikadasar006@gmail.com',
             'smtp_pass' => '1234asdf!@#$ASDF',
             'smtp_port' => 465,
             'mailtype' => 'html',
@@ -266,7 +266,7 @@ class Auth extends CI_Controller
         $this->load->library('email', $config);
         $this->email->initialize($config);
 
-        $this->email->from('fisikadasar001@gmail.com', 'Praktikum Fisika Dasar');
+        $this->email->from('fisikadasar006@gmail.com', 'Praktikum Fisika Dasar');
         $this->email->to($this->input->post('email', true));
 
         if ($type == 'verify') {
