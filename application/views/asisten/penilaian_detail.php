783b07095ec8f42c147af910a85521dc3471bf15
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-26 18:01:23 +0700

    debug

--- application/views/asisten/penilaian_detail.php
+++ application/views/asisten/penilaian_detail.php
@@ -7,7 +7,7 @@
         <!-- DataTales Example -->
         <div class="kotak">
             <div class="card-header">
-                <h6 class="m-0 font-weight-bold "><?= $title . "/" . $l['modul'] . "/" . $nama_kelompok; ?></h6>
+                <h6 class="m-0 font-weight-bold "><?= $title . "/" . $list['modul'] . "/" . $nama_kelompok; ?></h6>
             </div>
             <div class="card-body">
                 <div class="table-responsive">
