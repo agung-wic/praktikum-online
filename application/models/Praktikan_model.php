179a9f310dc7adb6d97c9f5ec6d58f57f4ef1fe1
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-26 21:35:17 +0700

    bug upload

--- application/models/Praktikan_model.php
+++ application/models/Praktikan_model.php
@@ -96,7 +96,7 @@ class Praktikan_model extends CI_Model
     $query = "SELECT `modul`.`modul`, `modul`.`name`, `jadwal`.`status`, `jadwal`.`nrp`,`nilai`.`is_acc` FROM `modul` 
               LEFT JOIN `jadwal` ON `modul`.`modul` = `jadwal`.`modul_id`
               LEFT JOIN `nilai` ON `jadwal`.`nrp` = `nilai`.`nrp`
-              WHERE `jadwal`.`nrp`=$id AND `nilai`.`is_acc`= 0;
+              WHERE `jadwal`.`nrp`=$id AND `nilai`.`is_acc`= 0
               ORDER BY `modul`.`modul` ASC";
 
     return $this->db->query($query)->result_array();
