2ba5b6e66864ee30be654508866726afbc6e8ef1
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-26 18:09:42 +0700

    debug

--- application/models/Asisten_model.php
+++ application/models/Asisten_model.php
@@ -3,18 +3,27 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Asisten_model extends CI_Model
 {
-  public function TampilNilai($id, $id_kelompok)
+  public function TampilNilai($id, $id_kelompok, $keyword = null)
   {
-    $query = "SELECT `nilai`.`id`, `user`.`name` as 'name_praktikan', `user`.`nrp`, `modul`.`modul` as 'modul_id', 
-    `modul`.`name` as 'modul', `nilai`.`laporan`, `nilai`.`laporan_time`, `nilai`.`is_acc` as 'is_acc', `nilai`.`asisten`, `nilai` . `resume`, `nilai` . `pretest`,`nilai` . `uji_lisan`,`nilai` . `praktikum`, `nilai` . `postest`,`nilai` . `format`,`nilai` . `bab`, `nilai` . `kesimpulan`, `nilai` . `nilai_akhir` as `nilai_akhir`, `nilai` . `nilai_akhir_abjad` as `nilai_akhir_abjad`,`anggota_kelompok`.`no_kelompok`as 'kelompok'
-    FROM `user`  
-    INNER JOIN `nilai` ON `user`.`nrp` = `nilai`.`nrp`
-    INNER JOIN `anggota_kelompok`ON `user`.`nrp` = `anggota_kelompok`.`nrp`
-
-    INNER JOIN `modul` ON `modul`.`modul` = `nilai`.`modul` WHERE `nilai` . `modul` = '$id' AND `anggota_kelompok`.`no_kelompok` = '$id_kelompok'";
-
-
-
+    if ($keyword) {
+      $query = "SELECT `nilai`.`id`, `user`.`name` as 'name_praktikan', `user`.`nrp`, `modul`.`modul` as 'modul_id', 
+              `modul`.`name` as 'modul', `nilai`.`laporan`, `nilai`.`laporan_time`, `nilai`.`is_acc` as 'is_acc', `nilai`.`asisten`, `nilai` . `resume`, `nilai` . `pretest`,`nilai` . `uji_lisan`,`nilai` . `praktikum`, `nilai` . `postest`,`nilai` . `format`,`nilai` . `bab`, `nilai` . `kesimpulan`, `nilai` . `nilai_akhir` as `nilai_akhir`, `nilai` . `nilai_akhir_abjad` as `nilai_akhir_abjad`,`anggota_kelompok`.`no_kelompok`as 'kelompok'
+              FROM `user`  
+              INNER JOIN `nilai` ON `user`.`nrp` = `nilai`.`nrp`
+              INNER JOIN `anggota_kelompok`ON `user`.`nrp` = `anggota_kelompok`.`nrp`
+              INNER JOIN `modul` ON `modul`.`modul` = `nilai`.`modul` 
+              WHERE (`nilai` . `modul` = '$id' AND `anggota_kelompok`.`no_kelompok` = '$id_kelompok') OR 
+              `user`.`name` LIKE '%$keyword%'
+              OR `user`.`nrp` LIKE '%$keyword%'";
+    } else {
+      $query = "SELECT `nilai`.`id`, `user`.`name` as 'name_praktikan', `user`.`nrp`, `modul`.`modul` as 'modul_id', 
+              `modul`.`name` as 'modul', `nilai`.`laporan`, `nilai`.`laporan_time`, `nilai`.`is_acc` as 'is_acc', `nilai`.`asisten`, `nilai` . `resume`, `nilai` . `pretest`,`nilai` . `uji_lisan`,`nilai` . `praktikum`, `nilai` . `postest`,`nilai` . `format`,`nilai` . `bab`, `nilai` . `kesimpulan`, `nilai` . `nilai_akhir` as `nilai_akhir`, `nilai` . `nilai_akhir_abjad` as `nilai_akhir_abjad`,`anggota_kelompok`.`no_kelompok`as 'kelompok'
+              FROM `user`  
+              INNER JOIN `nilai` ON `user`.`nrp` = `nilai`.`nrp`
+              INNER JOIN `anggota_kelompok`ON `user`.`nrp` = `anggota_kelompok`.`nrp`
+              INNER JOIN `modul` ON `modul`.`modul` = `nilai`.`modul` 
+              WHERE `nilai` . `modul` = '$id' AND `anggota_kelompok`.`no_kelompok` = '$id_kelompok'
+    }
     return $this->db->query($query)->result_array();
   }
 
