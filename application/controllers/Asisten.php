2b50deb51ccffad6ccb4b57a9619b303a4c6f937
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-26 23:42:37 +0700

    bug upload

--- application/controllers/Asisten.php
+++ application/controllers/Asisten.php
@@ -30,7 +30,7 @@ class Asisten extends CI_Controller
     $data['nama_modul'] = $this->db->get_where('modul', ['modul' =>  $data['id_modul']])->row_array();
     $data['user'] = $this->db->get_where('user', ['nrp' => $this->session->userdata('nrp')])->row_array();
     $data['title'] = 'Penilaian';
-    $data['kelompok'] = $this->Asisten_model->JumlahKelompok();
+    $data['kelompok'] = $this->Asisten_model->KelompokAsisten($this->session->userdata('nrp'));
     $this->load->view('template/header', $data);
     $this->load->view('template/sidebar', $data);
     $this->load->view('template/topbar', $data);
