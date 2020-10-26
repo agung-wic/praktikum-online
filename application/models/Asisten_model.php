a9a0b7f955516a8820bff098f24daa5ce993df89
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-26 20:34:27 +0700

    penilaian per modul per kelompok

--- application/models/Asisten_model.php
+++ application/models/Asisten_model.php
@@ -12,9 +12,9 @@ class Asisten_model extends CI_Model
               INNER JOIN `nilai` ON `user`.`nrp` = `nilai`.`nrp`
               INNER JOIN `anggota_kelompok`ON `user`.`nrp` = `anggota_kelompok`.`nrp`
               INNER JOIN `modul` ON `modul`.`modul` = `nilai`.`modul` 
-              WHERE (`nilai` . `modul` = '$id' AND `anggota_kelompok`.`no_kelompok` = '$id_kelompok') OR 
+              WHERE (`nilai` . `modul` = '$id' AND `anggota_kelompok`.`no_kelompok` = '$id_kelompok') AND (
               `user`.`name` LIKE '%$keyword%'
-              OR `user`.`nrp` LIKE '%$keyword%'";
+              OR `user`.`nrp` LIKE '%$keyword%')";
     } else {
       $query = "SELECT `nilai`.`id`, `user`.`name` as 'name_praktikan', `user`.`nrp`, `modul`.`modul` as 'modul_id', 
               `modul`.`name` as 'modul', `nilai`.`laporan`, `nilai`.`laporan_time`, `nilai`.`is_acc` as 'is_acc', `nilai`.`asisten`, `nilai` . `resume`, `nilai` . `pretest`,`nilai` . `uji_lisan`,`nilai` . `praktikum`, `nilai` . `postest`,`nilai` . `format`,`nilai` . `bab`, `nilai` . `kesimpulan`, `nilai` . `nilai_akhir` as `nilai_akhir`, `nilai` . `nilai_akhir_abjad` as `nilai_akhir_abjad`,`anggota_kelompok`.`no_kelompok`as 'kelompok'
