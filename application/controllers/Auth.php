a26e64576feacc4e86205a5720675506857f37e7
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-26 09:55:11 +0700

    debugging addemail

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -150,6 +150,8 @@ class Auth extends CI_Controller
                 ];
 
                 $user = $this->db->get_where('user', ['nrp' => $nrp])->row_array();
+                var_dump($data['name']);
+                die;
 
                 $this->db->insert('user_token', $user_token);
 
