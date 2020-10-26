8c849fdd97da7b18b7b7b9aac262f82548372007
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-26 16:25:30 +0700

    penilaian per modul per kelompok

--- application/views/asisten/penilaian.php
+++ application/views/asisten/penilaian.php
@@ -27,7 +27,6 @@
                                 <tr>
                                     <th scope="col">#</th>
                                     <th scope="col">Kelompok</th>
-                                    <th scope="col">Jumlah Anggota</th>
                                     <th scope="col">Action</th>
                                 </tr>
                             </thead>
@@ -38,7 +37,6 @@
 
                                         <th scope="row"><?= $i; ?></th>
                                         <td><?= $k['no_kelompok'] ?></td>
-                                        <td><?= $k['jumlah'] ?></td>
                                         <td>
                                             <a href="<?= base_url('modul/detail/' . $k['id']); ?>" class="badge badge-pill badge-primary"><i class="fas fa-fw fa-info"></i>Detail</a>
                                             <a href="<?= base_url('modul/editkelompok/' . $k['id']); ?>" data-id="<?= $k['id'] ?>" data-toggle="modal" data-target="#TambahKelompok" class="badge badge-pill badge-primary tombolEditKelompok"><i class="fas fa-fw fa-edit"></i>Edit</a>
