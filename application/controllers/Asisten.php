f7aabddbe80cc730b90c6394f79204c721e979fd
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-28 00:55:42 +0700

    kelompok praktikan bug

--- application/controllers/Asisten.php
+++ application/controllers/Asisten.php
@@ -49,6 +49,8 @@ class Asisten extends CI_Controller
     $data['user'] = $this->db->get_where('user', ['nrp' => $this->session->userdata('nrp')])->row_array();
     $data['list'] = $this->Asisten_model->TampilNilai($data['id_modul'], $data['id_kelompok'], $data['keyword']);
     $data['nama_kelompok'] = $this->db->get_where('kelompok', ['id' => $data['id_kelompok']])->row_array();
+    var_dump($data['nama_kelompok']);
+    die;
     $data['nama_modul'] = $this->db->get_where('modul', ['modul' =>  $data['id_modul']])->row_array();
     $i = 0;
     while ($i < count($data['list'])) {
