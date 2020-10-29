3e600e753d4db480d185c9d8827c698adbd036ec
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-30 00:43:34 +0700

    kelompok asisten

--- application/views/asisten/detailkelompok.php
+++ application/views/asisten/detailkelompok.php
@@ -10,7 +10,7 @@
             <div class="card-header">
                 <div class="row">
                     <div class="col">
-                        <h6 class="m-0 font-weight-bold "><?= $title . $nama_kelompok['no_kelompok']; ?></h6>
+                        <h6 class="m-0 font-weight-bold "><?= $nama_kelompok['no_kelompok']; ?></h6>
                     </div>
                     <div class="col-auto mr-auto">
                         <a href="" class="btn gradien mb-3 tombolTambahAnggota" data-id="<?= $id_kelompok ?>" data-toggle="modal" data-target="#TambahAnggota">Tambah Anggota</a>
