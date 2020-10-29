5103470ae0327aab747a4087977b7149f8812c4f
dws16 <dauuuussss@gmail.com>
2020-10-29 22:01:36 +0700

    debug

--- application/controllers/Modul.php
+++ application/controllers/Modul.php
@@ -484,7 +484,7 @@ class Modul extends CI_Controller
                 $no_kelompok = mb_convert_encoding($csv[0], "ISO-8859-1", "UTF-8");
                 $nrp = mb_convert_encoding($csv[1], "ISO-8859-1", "UTF-8");
                 if ($nrp == NULL || $nrp == "") {
-                    continue;
+                    break;
                 } else {
                     $id_kelompok = $this->db->get_where('kelompok', ['no_kelompok' => $no_kelompok])->row_array();
                     if ($id_kelompok) {
