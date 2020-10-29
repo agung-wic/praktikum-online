3cc2314afec67c1db98b4ec2ede117071aecad1d
dws16 <dauuuussss@gmail.com>
2020-10-29 21:19:31 +0700

    tes

--- application/controllers/Modul.php
+++ application/controllers/Modul.php
@@ -491,7 +491,7 @@ class Modul extends CI_Controller
                     } else {
                         $data = [
                             "nrp" => $nrp,
-                            "no_kelompok" => $id_kelompok
+                            "no_kelompok" => $id_kelompok['id']
                         ];
                         $this->db->insert('anggota_kelompok', $data);
                     }
