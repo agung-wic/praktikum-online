711fb24fd2a840a1c0357809dc84fe40acbb5e6a
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-30 03:02:47 +0700

    kelompok asisten perbaikan

--- application/models/Asisten_model.php
+++ application/models/Asisten_model.php
@@ -185,12 +185,12 @@ class Asisten_model extends CI_Model
   public function KelompokAsisten($id)
   {
     $query = "SELECT COUNT(`anggota_kelompok`.`nrp`) AS jumlah ,`kelompok`.`id` as `id_kelompok`, `kelompok_asisten`.`id` , `kelompok_asisten`.`nrp` as `nrp` , `kelompok_asisten`.`id_modul` as `id_modul` , `kelompok_asisten`.`no_kelompok` as `no_kelompok` ,
-              `user`.`name`,`kelompok`.`no_kelompok` FROM `kelompok_asisten` 
+              `user`.`name`,`kelompok`.`no_kelompok`,`modul`.`name` as 'name_modul' FROM `kelompok_asisten` 
               LEFT JOIN `user` ON `user`.`nrp` = `kelompok_asisten`.`nrp` 
               LEFT JOIN `kelompok` ON `kelompok`.`id` = `kelompok_asisten`.`no_kelompok` 
               LEFT JOIN `modul` ON `modul`.`modul` = `kelompok_asisten`.`id_modul` 
               LEFT JOIN `anggota_kelompok` ON `anggota_kelompok` . `no_kelompok` = `kelompok_asisten` . `no_kelompok`
-              WHERE `kelompok_asisten`.`nrp` = $id GROUP BY `kelompok_asisten`.`no_kelompok` ORDER BY `kelompok`.`id` ASC";
+              WHERE `kelompok_asisten`.`nrp` = $id GROUP BY `kelompok_asisten`.`no_kelompok` ORDER BY `kelompok_asisten`.`id_modul` ASC";
 
     return $this->db->query($query)->result_array();
   }
