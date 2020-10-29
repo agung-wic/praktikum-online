5a4e37f837376bc61b2dc23ffa1a0b8feb5ee932
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-29 23:41:49 +0700

    nrp spasi hapus

--- application/controllers/Modul.php
+++ application/controllers/Modul.php
@@ -483,6 +483,7 @@ class Modul extends CI_Controller
                 $csv = fgetcsv($data, 0, ';');
                 $no_kelompok = mb_convert_encoding($csv[0], "ISO-8859-1", "UTF-8");
                 $nrp = mb_convert_encoding($csv[1], "ISO-8859-1", "UTF-8");
+                $nrp = str_replace(" ", "", $nrp )
                 if ($csv[0] == NULL || $csv[0] == "") {
                     continue;
                 } else {
