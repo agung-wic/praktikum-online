c3caa28a6d750ed4f9c81678638eae795b3efb1d
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-30 04:47:33 +0700

    kelompok asisten perbaikan

--- application/controllers/Asisten.php
+++ application/controllers/Asisten.php
@@ -270,8 +270,8 @@ class Asisten extends CI_Controller
       'kesimpulan' => $this->input->post('kesimpulan', true),
       'asisten' => $this->session->userdata('nrp')
     ];
+    $id = $id;
     $modul_id = $this->input->post('modul_id', true);
-    $id_kelompok = $this->uri->segment(3);
     // $nilai_akhir = (($data['resume'] * 0.25) + ($data['pretest'] * 0.05) + ($data['uji_lisan'] * 0.1) + ($data['praktikum'] * 0.1) + ($data['postest'] * 0.05) + ($data['format'] * 0.1) + ($data['bab'] * 0.25) + ($data['kesimpulan'] * 0.1));
     $nilai_akhir = (($data['resume']) + ($data['pretest']) + ($data['uji_lisan']) + ($data['praktikum']) + ($data['postest']) + ($data['format']) + ($data['bab']) + ($data['kesimpulan']));
     if ($nilai_akhir >= 86 && $nilai_akhir <= 100) {
@@ -296,6 +296,9 @@ class Asisten extends CI_Controller
     $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
           Nilai berhasil diubah!
           </div>');
+    var_dump($id);
+    var_dump($id_kelompok);
+    die;
     redirect(base_url('asisten/penilaian_detai/' . $modul_id . "/" . $id_kelompok));
   }
 
