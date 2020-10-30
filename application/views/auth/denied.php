3299e41ac6dce3f8951e9a862d0e37dddbab8785
dws16 <dauuuussss@gmail.com>
2020-10-30 17:41:21 +0700

    ok

--- application/views/auth/denied.php
+++ application/views/auth/denied.php
@@ -5,7 +5,7 @@
   <div class="text-center">
     <div class="error mx-auto" data-text="403">403</div>
     <p class="lead text-gray-800 mb-5">Access Forbidden</p>
-    <p class="text-gray-500 mb-0">Sedang berlangsung maintenance</p>
+    <p class="text-gray-800 mb-0">Sedang berlangsung maintenance</p>
     <!-- <a href="<?= base_url('profile') ?>">&larr; Back to Dashboard</a> -->
   </div>
 
