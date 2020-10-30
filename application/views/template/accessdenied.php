f9089d254dc0b52735522224ee83cc1399aa49d4
mpuhambyah <mpu.hambyah@gmail.com>
2020-10-31 05:52:58 +0700

    akses edit navigasi admin saja edit

--- application/views/template/accessdenied.php
+++ application/views/template/accessdenied.php
@@ -2,7 +2,7 @@
 <div class="container-fluid mt-5">
 
     <!-- 404 Error Text -->
-    <div class="text-center">
+    <div class="text-center" style="margin-top: 10%;">
         <div class="error mx-auto" data-text="404">404</div>
         <p class="lead text-gray-800 mb-5">Access Forbidden</p>
         <p class="text-gray-800 mb-0">Tidak Diperbolehkan</p>
