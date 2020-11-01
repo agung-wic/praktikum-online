0ae2d140e5e931337b497beda28ef28c334bf26c
mpuhambyah <mpu.hambyah@gmail.com>
2020-11-01 21:08:49 +0700

    laporan maintenance

---
+++ application/views/template/laporanbelum.php
@@ -0,0 +1,18 @@
+<!-- Begin Page Content -->
+<div class="container-fluid mt-5">
+
+    <!-- 404 Error Text -->
+    <div class="text-center" style="margin-top: 10%;">
+        <div class="error mx-auto" data-text="404">404</div>
+        <p class="lead text-gray-800 mb-5">Access Forbidden</p>
+        <p class="text-gray-800 mb-0">Tidak Diperbolehkan</p>
+
+        <p class="text-gray-800 mb-0">Belum diperbolehkan upload disini, tunggu intruksi dari asisten</p>
+        <!-- <a href="<?= base_url('profile') ?>">&larr; Back to Dashboard</a> -->
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
