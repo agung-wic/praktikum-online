7fc83ef05c6b754fb5df8bc739ddc361b6e6bfa0
mpuhambyah <mpu.hambyah@gmail.com>
2020-11-02 10:24:36 +0700

    absen

--- application/controllers/Praktikan.php
+++ application/controllers/Praktikan.php
@@ -349,7 +349,7 @@ class Praktikan extends CI_Controller
                 return $success;
                 socket_close($socket2);
             } else {
-                echo "<script>alert('Tidak dapat terhubung ke server!');
+                echo "<script>alert('Absen Berhasil! Silahkan tekan OK!');
                 window.location.href='" . base_url('praktikan/modul/') . $id . "';</script>";
             }
         } else {
