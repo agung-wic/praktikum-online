50a1fc73e741afed1b55b06b3cc77eb16266a60e
mpuhambyah <mpu.hambyah@gmail.com>
2020-11-04 07:59:02 +0700

    search

--- application/models/Asisten_model.php
+++ application/models/Asisten_model.php
@@ -6,7 +6,7 @@ class Asisten_model extends CI_Model
   public function TampilNilai($id, $id_kelompok, $keyword = null)
   {
     if ($keyword) {
-      $query = "SELECT `nilai`.`id`, `user`.`name` as 'name_praktikan', `user`.`nrp`, `modul`.`modul` as 'modul_id', 
+      $query = "SELECT `nilai`.`id`, `user`.`name` as 'name_praktikan', `user`.`nrp` as 'nrp', `modul`.`modul` as 'modul_id', 
               `modul`.`name` as 'modul', `nilai`.`laporan`, `nilai`.`laporan_time`, `nilai`.`is_acc` as 'is_acc', `nilai`.`asisten`, `nilai` . `resume`, `nilai` . `pretest`,`nilai` . `uji_lisan`,`nilai` . `praktikum`, `nilai` . `postest`,`nilai` . `format`,`nilai` . `bab`, `nilai` . `kesimpulan`, `nilai` . `nilai_akhir` as `nilai_akhir`, `nilai` . `nilai_akhir_abjad` as `nilai_akhir_abjad`,`anggota_kelompok`.`no_kelompok`as 'kelompok'
               FROM `user`  
               INNER JOIN `nilai` ON `user`.`nrp` = `nilai`.`nrp`
