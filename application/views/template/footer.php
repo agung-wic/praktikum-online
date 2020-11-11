443ae29423fb3aa474d85934499f63d0aba96e0c
dws16 <dauuuussss@gmail.com>
2020-11-11 13:11:26 +0700

    debug

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -116,7 +116,7 @@
     absen.prop('disabled', true);
   });
 </script>
-<script src="<?= base_url(); ?>assets/js/script.js?v=108"></script>
+<script src="<?= base_url(); ?>assets/js/script.js?v=109"></script>
 
 
 </body>
