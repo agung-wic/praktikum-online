a44db10c2b2610a438f53592403062431d543d70
mpuhambyah <mpu.hambyah@gmail.com>
2020-11-17 08:44:26 +0700

    admin nilai

--- application/models/Asisten_model.php
+++ application/models/Asisten_model.php
@@ -211,7 +211,13 @@ class Asisten_model extends CI_Model
 
   public function KelompokAsistenNilaiAdmin($id_modul)
   {
-    $query = "SELECT * FROM `nilai` WHERE `nilai`.`modul` = '$id_modul'";
+    $query = "SELECT COUNT(`anggota_kelompok`.`nrp`) AS jumlah ,`kelompok`.`id` as `id_kelompok`, `kelompok_asisten`.`id` , `kelompok_asisten`.`nrp` as `nrp` , `kelompok_asisten`.`id_modul` as `id_modul` , `kelompok_asisten`.`no_kelompok` as `no_kelompok` ,
+              `user`.`name`,`kelompok`.`no_kelompok`,`modul`.`name` as 'name_modul' FROM `kelompok_asisten` 
+              LEFT JOIN `user` ON `user`.`nrp` = `kelompok_asisten`.`nrp` 
+              LEFT JOIN `kelompok` ON `kelompok`.`id` = `kelompok_asisten`.`no_kelompok` 
+              LEFT JOIN `modul` ON `modul`.`modul` = `kelompok_asisten`.`id_modul` 
+              LEFT JOIN `anggota_kelompok` ON `anggota_kelompok` . `no_kelompok` = `kelompok_asisten` . `no_kelompok`
+              WHERE `kelompok_asisten`.`id_modul` = '$id_modul' GROUP BY `kelompok_asisten`.`no_kelompok` ORDER BY `kelompok_asisten`.`id_modul` ASC";
 
     return $this->db->query($query)->result_array();
   }
