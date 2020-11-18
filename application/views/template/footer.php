c0e12245753b2eb8682a3756f1cf4436ac5a49aa
mpuhambyah <mpu.hambyah@gmail.com>
2020-11-18 16:20:48 +0700

    asisten menyelesaikan jadwal

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -99,7 +99,7 @@
     absen.prop('disabled', true);
   });
 </script>
-<script src="<?= base_url(); ?>assets/js/script.js?v=122"></script>
+<script src="<?= base_url(); ?>assets/js/script.js?v=123"></script>
 
 
 </body>
