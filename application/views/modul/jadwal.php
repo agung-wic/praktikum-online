a5b8e34d06eb2c4b6bdbad674a16fa1d89ddd3f2
mpuhambyah <mpu.hambyah@gmail.com>
2020-11-18 14:59:26 +0700

    asisten menyelesaikan jadwal

--- application/views/modul/jadwal.php
+++ application/views/modul/jadwal.php
@@ -121,21 +121,7 @@
             <div class="table-responsive">
                 <div class="row mx-1">
                     <div class="col-md-7">
-                        <div class="row">
-                            <div class="col-md-1 mt-2">
-                                <label for="show">Show</label>
-                            </div>
-                            <div class="col-md-2">
-                                <select class="custom-select" name="show" id="show">
-                                    <option value="10">10</option>
-                                    <option value="20">20</option>
-                                    <option value="30">30</option>
-                                    <option value="40">40</option>
-                                    <option value="50">50</option>
-                                </select>
-                            </div>
-                            <div class="col-md-2 mt-2">Entries</div>
-                        </div>
+
                     </div>
                     <div class="col-md-5 ml-auto">
                         <form action="<?= base_url('modul/jadwal') ?>" method="post">
@@ -289,6 +275,20 @@
                     <div class="form-group">
                         <label for="jadwal">Jadwal</label>
                         <input type="datetime-local" class="form-control" id="jadwal" name="jadwal">
+                    </div>
+                    <div class="form-group">
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="status" id="selesai" value="selesai" checked>
+                            <label class="form-check-label" for="status">
+                                Selesai
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="status" id="belum_selesai" value="belum_selesai">
+                            <label class="form-check-label" for="status">
+                                Belum Selesai
+                            </label>
+                        </div>
                     </div>
             </div>
             <div class="modal-footer">
