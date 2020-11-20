b46c015329c83b9d27166642d52c7b96394b8cdb
mpuhambyah <mpu.hambyah@gmail.com>
2020-11-20 17:15:12 +0700

    debug

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -99,7 +99,7 @@
     absen.prop('disabled', true);
   });
 </script>
-<script src="<?= base_url(); ?>assets/js/script.js?v=126"></script>
+<script src="<?= base_url(); ?>assets/js/script.js?v=127"></script>
 
 
 </body>
