626aca20d15d34091adb508e4355578b49e36c52
mpuhambyah <mpu.hambyah@gmail.com>
2020-11-21 00:52:58 +0700

    operator praktikan

--- application/views/praktikan/percobaan.php
+++ application/views/praktikan/percobaan.php
@@ -137,7 +137,7 @@
 
 <div id="mydiv">
   <div class="container p-5">
-    <div class="kotak row" id="mydivheader" style="background-color:white;opacity:75%;width:60%;transform:scale(0.7)">
+    <div class="kotak row" id="mydivheader" style="background-color:white;position:fixed;opacity:75%;width:60%;transform:scale(0.7)">
       <div class="kotak col-lg-4 mb-0" style="background-color: #bcaead;   
           border-top-left-radius: 25px;
           border-top-right-radius: 0px;
