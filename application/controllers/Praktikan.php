a0256ee1d8cfdda7385d51a2f619a140e5971cec
mpuhambyah <mpu.hambyah@gmail.com>
2020-11-23 21:17:47 +0700

    koefisien error

--- application/controllers/Praktikan.php
+++ application/controllers/Praktikan.php
@@ -379,7 +379,7 @@ class Praktikan extends CI_Controller
                         } else if (($i + 3) % 4 == 0) {
                             $result4[$i] = $result2[$i];
                         } else if (($i + 2) % 4 == 0) {
-                            $result5[$i] = $result2[$i] * 0.001;
+                            $result5[$i] = $result2[$i];
                             $result5[$i] = number_format($result5[$i], 2);
                         } else if (($i + 1) % 4 == 0) {
                             $result6[$i] = $result2[$i] * 0.001;
