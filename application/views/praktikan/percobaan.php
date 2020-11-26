fc55d61d3380bf1df7a0d6886edb808fe0e96aae
mpuhambyah <mpu.hambyah@gmail.com>
2020-11-26 18:53:00 +0700

    tombol reset alata

--- application/views/praktikan/percobaan.php
+++ application/views/praktikan/percobaan.php
@@ -148,7 +148,7 @@
             <label for="data1">Status</label>
             <output style="width:180px;margin-left:41%" type="text" id="reset" name="reset" class="form-control form-control-user mb-4"></output>
             <div class="row justify-content-center mb-1">
-              <button type="submit" style="margin: 1%;width:150px" data-tampil="#reset" data-kirim="[reset]" data-id="<?= $modul['modul'] ?>" class="param1 btn btn-danger px-4 kirim1a">
+              <button type="submit" style="margin: 1%;width:150px" data-tampil="#reset" data-kirim="[a]" data-id="<?= $modul['modul'] ?>" class="param1 btn btn-danger px-4 kirim1a">
                 Reset Alat
               </button>
             </div>
