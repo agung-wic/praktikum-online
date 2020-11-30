e389c8f032422d7dca4261c33f59a0c2d8ea0e40
mpuhambyah <mpu.hambyah@gmail.com>
2020-11-30 15:17:10 +0700

    bug footer logout

--- application/views/asisten/penilaian_detail.php
+++ application/views/asisten/penilaian_detail.php
@@ -123,27 +123,104 @@
 
 
 
-<!-- Modal -->
-<div class="modal fade" id="TambahKelompok" tabindex="-1" role="dialog" aria-labelledby="TambahKelompokLabel" aria-hidden="true">
+
+<div class="modal fade" id="NilaiEdit" tabindex="-1" role="dialog" aria-labelledby="NilaiEditLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="TambahKelompokLabel"></h5>
+                <h5 class="modal-title" id="NilaiEditLabel">Edit Nilai Praktikan</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div class="modal-body">
-                <form action="<?= base_url('asisten/tambahkelompok') ?>" method="post">
+            <form action="<?= base_url('asisten/editnilai/') . $l['id'] . "/" . $id_kelompok ?>" method="post">
+                <div class="modal-body">
                     <div class="form-group">
-                        <input type="hidden" id="id" name="id">
-                        <input type="text" class="form-control" id="no_kelompok" name="no_kelompok" placeholder="">
+                        <input class="form-control" type="text" name="id" id="id" hidden>
+                        <input class="form-control" type="text" name="modul_id" id="modul_id" hidden>
                     </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary">Add</button>
-            </div>
+                    <div class="form-group hapus">
+                        <label for="name">Nama Lengkap</label>
+                        <input type="text" class="form-control " id="name" name="name" disabled>
+                    </div>
+                    <div class="form-group hapus">
+                        <label for="nrp">NRP</label>
+                        <input type="text" class="form-control " id="nrp" name="nrp" disabled>
+                    </div>
+                    <div class="form-group hapus">
+                        <label for="modul">Modul</label>
+                        <input type="text" class="form-control " id="modul" name="modul" disabled>
+                    </div>
+                    <div class="form-group">
+                        <label for="resume">Resume (0-25)</label>
+                        <input type="number" min="0" max="25" class="form-control" id="resume" name="resume">
+                    </div>
+                    <div class="form-group">
+                        <label for="pretest">Pretest (0-5)</label>
+                        <input type="number" min="0" max="5" class="form-control" id="pretest" name="pretest">
+                    </div>
+                    <div class="form-group">
+                        <label for="uji_lisan">Uji Lisan (0-10)</label>
+                        <input type="number" min="0" max="10" class="form-control" id="uji_lisan" name="uji_lisan">
+                    </div>
+                    <div class="form-group">
+                        <label for="praktikum">Praktikum (0-10)</label>
+                        <input type="number" min="0" max="10" class="form-control" id="praktikum" name="praktikum">
+                    </div>
+                    <div class="form-group">
+                        <label for="postest">Postest/Video (0-5)</label>
+                        <input type="number" min="0" max="5" class="form-control" id="postest" name="postest">
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="form-group">
+                                <label for="laporan">Laporan</label>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <span>Format (0-10)</span>
+                                    </div>
+                                    <div class="col-4">
+                                        <input type="number" min="0" max="10" class="form-control mb-1" id="format" name="format" placeholder="Format Penulisan">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <span>Bab (0-25)</span>
+                                    </div>
+                                    <div class="col-4">
+                                        <input type="number" min="0" max="25" class="form-control mb-1" id="bab" name="bab" placeholder="Bab 4, 4, 4">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <span>Kesimpulan (0-10)</span>
+                                    </div>
+                                    <div class="col-4">
+                                        <input type="number" min="0" max="10" class="form-control" id="kesimpulan" name="kesimpulan" placeholder="Kesimpulan">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="form-group">
+                                <label for="laporan">Nilai Akhir</label>
+                                <div class="row">
+                                    <div class="col-auto">
+                                        <input type="number" min="0" max="100" class="form-control mb-1" id="nilai_akhir" name="nilai_akhir" disabled>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-auto">
+                                        <input type="text" min="0" max="100" class="form-control mb-1" id="nilai_akhir_abjad" name="nilai_akhir_abjad" disabled>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary hapus">Edit</button>
+                    </div>
             </form>
         </div>
     </div>
