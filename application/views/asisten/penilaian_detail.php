7234c8510d9db76a3d2d5ed5a11ad12c9c063be8
dws16 <dauuuussss@gmail.com>
2020-11-30 11:39:31 +0700

    tes

--- application/views/asisten/penilaian_detail.php
+++ application/views/asisten/penilaian_detail.php
@@ -60,7 +60,9 @@
                                 </tr>
                             </tfoot>
                             <tbody>
+                                <?php foreach ($list as $l) : ?>
 
+                                <?php endforeach; ?>
                             </tbody>
                         </table>
                     <?php } ?>
