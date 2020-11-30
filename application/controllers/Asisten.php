96c94a915ed3c0ffd2c5656710e1b5856635626d
mpuhambyah <mpu.hambyah@gmail.com>
2020-11-30 12:51:28 +0700

    kelompok bug

--- application/controllers/Asisten.php
+++ application/controllers/Asisten.php
@@ -122,8 +122,6 @@ class Asisten extends CI_Controller
       $data['kelompok'] = $this->Asisten_model->KelompokAsistenAdmin();
     } else {
       $data['kelompok'] = $this->Asisten_model->KelompokAsisten($this->session->userdata('nrp'));
-      var_dump($data['kelompok']);
-      die;
     }
     $this->load->view('template/header', $data);
     $this->load->view('template/sidebar', $data);
