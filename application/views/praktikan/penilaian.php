a3b31a15aebb174b5f47d7ff82f8774a03d1b266
mpuhambyah <mpu.hambyah@gmail.com>
2020-12-08 20:55:52 +0700

    bug penilaian

--- application/views/praktikan/penilaian.php
+++ application/views/praktikan/penilaian.php
@@ -51,6 +51,7 @@
                       var_dump(count($m['modul']));
                       var_dump(count($list[$i]['modul_id']));
                       die;
+                    }
                       if ($m['modul'] == $list[$i]['modul_id']) {
                     ?>
                         <td><?= $list[$i]['asisten']; ?></td>
