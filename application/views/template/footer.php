ebabad0b152f4e0558efbc593f2ecc0afbb556c0
dws16 <dauuuussss@gmail.com>
2021-01-30 09:48:03 +0700

    tes

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -27,7 +27,7 @@
 <script src="<?= base_url(); ?>assets/vendor/jquery-easing/jquery.easing.min.js"></script>
 
 <!-- Custom scripts for all pages-->
-<script src="<?= base_url(); ?>assets/js/sb-admin-2.min.js?v=10"></script>
+<script src="<?= base_url(); ?>assets/js/sb-admin-2.min.js?v=11"></script>
 <script>
   tinymce.init({
     selector: "textarea.edit",
